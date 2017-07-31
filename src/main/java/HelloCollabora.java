84ea9c8ece7ac62f1ea2151690a9ec8c57e464f8
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-07-31 16:43:35 +0200

    First commit

---
+++ src/main/java/HelloCollabora.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mperuzzi on 31/07/17.
+ */
+public class HelloCollabora {
+
+    public String printHelloCollabora(final int num) {
+        return "Hello " + num + " times from Collabora";
+    }
+
+}

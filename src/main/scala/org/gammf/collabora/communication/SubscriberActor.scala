b4528e9444ab931c0a8acc44721ae298208ccb2a
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-03 11:32:16 +0200

    First sketch of a subscriber actor

---
+++ src/main/scala/org/gammf/collabora/communication/SubscriberActor.scala
@@ -0,0 +1,16 @@
+package org.gammf.collabora.communication
+
+import akka.actor.Actor
+
+/**
+  * Created by mperuzzi on 03/08/17.
+  */
+class SubscriberActor extends Actor {
+
+  override def receive = {
+    case SubscribeMessage =>
+      println("Subscribe message received!")
+    case _ => println("Huh?")
+  }
+
+}

7a086106ea32138dff38991b454bd2b678ed89f8
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2017-08-04 15:36:49 +0200

    create a simple Test class for testing actor system

--- src/main/scala/org/gammf/collabora/Test.scala
+++ src/main/scala/org/gammf/collabora/Test.scala
@@ -1,81 +1,20 @@
 package org.gammf.collabora
 
-import akka.actor.{Actor, ActorSystem, Props}
-import org.gammf.collabora.database.DBActor
+import java.util.Calendar
+
+import akka.actor.{ActorSystem, Props}
+import org.gammf.collabora.database.actors.{ConnectionManagerActor, DBActor, PrintActor}
 import org.gammf.collabora.database.messages.{InsertNoteMessage, RequestAllNotesMessage}
-import org.gammf.collabora.util.Note
-import reactivemongo.api.collections.bson.BSONCollection
-import reactivemongo.api.{Cursor, FailoverStrategy, MongoConnection, MongoDriver}
-import reactivemongo.bson.{BSONArray, BSONDocument}
-import reactivemongo.play.json._
-import play.api.libs.json._
+import org.gammf.collabora.util.SimpleNote
 
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
-import scala.util.Success
-import scala.util.Failure
-
-class Test extends Actor {
-
-  val mongoUri = "mongodb://localhost:27017/collabora?authMode=scram-sha1"
-
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  val driver = MongoDriver()
-  val parseUri = MongoConnection.parseURI(mongoUri)
-  val connection = parseUri.map(driver.connection(_))
-
-  def collFut: Future[BSONCollection] = connection.get.database("collabora", FailoverStrategy())
-    .map(_.collection("collaboration", FailoverStrategy()))
-  val users: BSONCollection = Await.result(collFut, 10 seconds)
-
-  def getUser(username: String): Future[List[BSONDocument]]= {
-    // { "age": { "$gt": 27 } }
-    val query = BSONDocument("users" -> BSONDocument("$elemMatch" -> BSONDocument("$eq" -> username)))
-
-    // MongoDB .findOne
-    users.find(query).cursor[BSONDocument]().collect[List](10, Cursor.FailOnError[List[BSONDocument]]())
-  }
-
-  override def receive: Receive = {
-    case "asd" => println("HAHA")
-    case name: String => {
-      val us = getUser(name)
-      us onComplete {
-        case Success(l) => l.foreach(e => println(BSONFormats.BSONDocumentFormat.writes(e).as[JsObject]))
-        case Failure(e) => println("Error " + e)
-      }
-    }
-    case _ => println("NO")
-
-  }
-}
-
-object Main extends App {
-  /*val system = ActorSystem("HelloSystem")
-  // default Actor constructor
-  val helloActor = system.actorOf(Props[Test], name = "helloactor")
-  helloActor ! "manuelperuzzi"*/
-
-
-  val mongoUri = "mongodb://localhost:27017/collabora?authMode=scram-sha1"
-
-  val driver = MongoDriver()
-  val parseUri = MongoConnection.parseURI(mongoUri)
-  val connection = parseUri.map(driver.connection(_))
-
+object Test extends App {
   val system = ActorSystem("HelloSystem")
-  // default Actor constructor
-  val dbActor = system.actorOf(Props.create(classOf[DBActor], connection.get))
+  val connectionActor = system.actorOf(Props[ConnectionManagerActor])
+  val printActor = system.actorOf(Props[PrintActor])
+  val dbActor = system.actorOf(Props.create(classOf[DBActor], connectionActor, printActor))
 
-  val note: Note = new Note(content = "Insertion test", state = "toDo", location = Some(13.2541, 45.2541))
+  val note: SimpleNote = new SimpleNote(content = "Insertion test", state = "toDo", location = Some(13.2541, 45.2541), previousNotes = Some(List("5980710df27da3fcfe0ac88d", "59806ff7f27da3fcfe0ac7d2")), expiration = Some(Calendar.getInstance.getTime))
 
   dbActor ! new InsertNoteMessage(note)
   dbActor ! new RequestAllNotesMessage()
-  dbActor ! new RequestAllNotesMessage()
-  dbActor ! new RequestAllNotesMessage()
-  dbActor ! new RequestAllNotesMessage()
-  dbActor ! new RequestAllNotesMessage()
-  dbActor ! new RequestAllNotesMessage()
-
 }

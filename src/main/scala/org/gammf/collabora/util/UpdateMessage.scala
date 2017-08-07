a5810469e7f13351e74a2142248b10c6647927cb
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2017-08-07 22:57:29 +0200

    add update message trait and class

---
+++ src/main/scala/org/gammf/collabora/util/UpdateMessage.scala
@@ -0,0 +1,11 @@
+package org.gammf.collabora.util
+
+/**
+  * A very simple trait that represent an (incomplete, for now) update message
+  */
+trait UpdateMessage {
+  def target: String
+  def messageType: String
+  def user: String
+  def note: Note
+}

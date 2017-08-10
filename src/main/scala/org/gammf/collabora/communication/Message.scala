7d63e80b543a428939104b39af1c3848059e0870
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-10 10:53:51 +0200

    Added a publish message containing the information to be sent to the user

--- src/main/scala/org/gammf/collabora/communication/Message.scala
+++ src/main/scala/org/gammf/collabora/communication/Message.scala
@@ -81,6 +81,13 @@ case class ClientUpdateMessage(text: String) extends Message
 /**
   * Represents a notification message to be published.
   * @param collaborationID the identifier of the collaboration to which the message is addressed.
-  * @param message the text of the message to be published.
+  * @param message the text of the message to be published in json format.
   */
 case class PublishNotificationMessage(collaborationID: String, message: JsValue) extends Message
+
+/**
+  * Represents a message sent to a user that has just been added to a collaboration.
+  * @param username the identifier of the user to which the message is addressed.
+  * @param message the text of the message to be published in json format.
+  */
+case class PublishMemberAddedMessage(username: String, message: JsValue) extends Message

f6a4fb91fe2d10b1717b13a4c6482b6d0d9a6679
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-10 11:15:13 +0200

    Added a simple use case of collaboration members actor

--- src/main/scala/org/gammf/collabora/communication/CollaborationMembersActor.scala
+++ src/main/scala/org/gammf/collabora/communication/CollaborationMembersActor.scala
@@ -1,7 +1,8 @@
 package org.gammf.collabora.communication
 
-import akka.actor.{Actor, ActorRef, Stash}
-import com.newmotion.akka.rabbitmq.Channel
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Stash}
+import com.newmotion.akka.rabbitmq.{Channel, ConnectionActor, ConnectionFactory}
+import play.api.libs.json.Json
 
 /**
   * @author Manuel Peruzzi
@@ -37,5 +38,52 @@ class CollaborationMembersActor(connection: ActorRef, naming: ActorRef, channelC
 
     case _ => println("[CollaborationMembersActor] Huh?")
   }
+}
 
+/**
+  * This is a simple application that uses the Collaboration Members Actor.
+  */
+object UseCollaborationMembersActor extends App {
+  implicit val system = ActorSystem()
+  val factory = new ConnectionFactory()
+  val connection = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
+
+  val naming = system.actorOf(Props[RabbitMQNamingActor], "naming")
+  val channelCreator = system.actorOf(Props[ChannelCreatorActor], "channelCreator")
+  val publisher = system.actorOf(Props[PublisherActor], "publisher")
+  val collaborationMember = system.actorOf(Props(
+    new CollaborationMembersActor(connection, naming, channelCreator, publisher)), "collaboration-members")
+
+  val message = """{
+      "user": "manuelperuzzi",
+      "collaboration": {
+        "id": "arandomidofarandomcollaboration",
+        "name": "random-collaboration",
+        "collaborationType": "group",
+        "users": [
+          {
+            "username": "manuelperuzzi",
+            "email": "manuel.peruzzi@studio.unibo.it",
+            "name": "Manuel",
+            "surname": "Peruzzi"
+            "right": "admin"
+          }
+        ],
+        "notes": [
+          {
+            "id": "arandomidofarandomnote",
+            "content": "some content",
+            "state": {
+              "definition": "doing",
+              "username": "manuelperuzzi"
+            }
+          }
+        ]
+      }
+  }"""
+
+  collaborationMember ! PublishMemberAddedMessage("maffone", Json.toJson(message))
+
+  Thread.sleep(1000)
+  collaborationMember ! StartMessage
 }

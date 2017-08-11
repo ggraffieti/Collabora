2408c3afd9f1d87b5871e8640cac016ec3376945
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-11 18:32:47 +0200

    Added a simple java test class to send notifications to clients through firebase service

---
+++ src/main/java/org/gammf/collabora/testClasses/FirebaseNotificationSender.java
@@ -0,0 +1,24 @@
+package org.gammf.collabora.testClasses;
+
+public class FirebaseNotificationSender {
+
+    private static final String FIREBASE_URI = "https://fcm.googleapis.com/fcm/send";
+    private static final String AUTHORIZATION = "AAAAJtSw2Gk:APA91bEXmB5sRFqSnuYIP3qofHQ0RfHrAzTllJ0vYWtHXKZsMdbuXmUKbr16BVZsMO0cMmm_BWE8oLzkFcyuMr_V6O6ilqvLu7TrOgirVES51Ux9PsKfJ17iOMvTF_WtwqEURqMGBbLf";
+    private static final String NOTIFICATION_TITLE = "collabora-project";
+    private static final String NOTIFICATION_BODY = "Peru inserted a note";
+    private static final String TOPIC = "collabora-project-id";
+
+    public static void main(String[] args) throws Exception {
+
+        final HttpPost post = new HttpPost(FIREBASE_URI);
+        post.setHeader("Authorization", "key=" + AUTHORIZATION);
+        post.setHeader("Content-Type", "application/json");
+        final String payload = "{\"notification\": { \"title\": \"" + NOTIFICATION_TITLE + "\", \"body\": \"" + NOTIFICATION_BODY + "\"}, \"to\" : \"/topics/" + TOPIC + "\"}";
+        post.setEntity(new StringEntity(payload));
+
+        final HttpClient http = HttpClientBuilder.create().build();
+        final HttpResponse response = http.execute(post);
+        System.out.println(response);
+    }
+
+}

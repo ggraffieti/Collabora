18357d11550c02b0ea85fc7cb3b31df6866a634f
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2017-08-12 23:31:49 +0200

    delete useless imports and add parentheses in toString

--- src/main/scala/org/gammf/collabora/util/Module.scala
+++ src/main/scala/org/gammf/collabora/util/Module.scala
@@ -1,8 +1,5 @@
 package org.gammf.collabora.util
 
-import org.joda.time.DateTime
-import reactivemongo.bson.{BSONDocument, BSONDocumentReader, BSONObjectID}
-
 trait Module {
 
   def id: Option[String]
@@ -11,11 +8,11 @@ trait Module {
   def state: String
 
 
-  override def toString: String =
+  override def toString: String = {
     "{ Module -- id=" + id +
       ", content=" + content +
       ", previousModules=" + previousModules +
       ", state=" + state
-      " }"
-
+    " }"
+  }
 }

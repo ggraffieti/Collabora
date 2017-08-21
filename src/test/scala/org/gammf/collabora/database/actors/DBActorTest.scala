92dde6dd843bb914053850f83db13015dd1a43da
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-08-21 19:27:59 +0200

    searching for methods to fully test actors

--- src/test/scala/org/gammf/collabora/database/actors/DBActorTest.scala
+++ src/test/scala/org/gammf/collabora/database/actors/DBActorTest.scala
@@ -2,13 +2,27 @@ package org.gammf.collabora.database.actors
 
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
-import org.gammf.collabora.database.messages.{AskConnectionMessage, GetConnectionMessage}
+import com.newmotion.akka.rabbitmq.{ConnectionActor, ConnectionFactory}
+import org.gammf.collabora.communication.actors._
+import org.gammf.collabora.database.messages.{AskConnectionMessage, GetConnectionMessage, InsertNoteMessage}
+import org.gammf.collabora.util.UpdateMessageImpl
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
 import scala.concurrent.duration._
 
 class DBActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpecLike  with Matchers with BeforeAndAfterAll with ImplicitSender {
 
   val dbConnectionActor: ActorRef = system.actorOf(Props[ConnectionManagerActor])
+  val factory = new ConnectionFactory()
+  val connection:ActorRef = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
+  val naming: ActorRef = system.actorOf(Props[RabbitMQNamingActor], "naming")
+  val channelCreator: ActorRef = system.actorOf(Props[ChannelCreatorActor], "channelCreator")
+  val publisherActor: ActorRef = system.actorOf(Props[PublisherActor], "publisher")
+  val notificationActor: ActorRef = system.actorOf(Props(new NotificationsSenderActor(connection, naming, channelCreator, publisherActor)))
+  val dbActor: ActorRef = system.actorOf(Props.create(classOf[DBActor], dbConnectionActor, notificationActor))
+  val subscriber:ActorRef = system.actorOf(Props[SubscriberActor], "subscriber")
+  val updatesReceiver:ActorRef = system.actorOf(Props(
+    new UpdatesReceiverActor(connection, naming, channelCreator, subscriber, dbActor)), "updates-receiver")
 
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
@@ -19,6 +33,12 @@ class DBActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpec
       within(500 millis) {
         dbConnectionActor ! new AskConnectionMessage()
         expectMsgType[GetConnectionMessage]
+      }
+    }
+
+    "insert note in the db and after that, send a publish to NotificationActor" in {
+      within(500 millis) {
+        //dbActor ! new InsertNoteMessage()
       }
     }
   }

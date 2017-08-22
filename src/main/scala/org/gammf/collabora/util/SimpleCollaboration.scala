84fc91fb222f3007eafe9e55fc348a105fbfcc24
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-08-22 17:13:24 +0200

    Collaboration and SimpleCollaboration refactoring, implicit conversions now work with trait instead of case class

--- src/main/scala/org/gammf/collabora/util/SimpleCollaboration.scala
+++ src/main/scala/org/gammf/collabora/util/SimpleCollaboration.scala
@@ -17,69 +17,9 @@ import reactivemongo.bson.{BSON, BSONArray, BSONDocument, BSONDocumentReader, BS
   */
 case class SimpleCollaboration(id: Option[String] = None, name: String, collaborationType: CollaborationType,
                                users: Option[List[CollaborationUser]] = None,
-                               modules: Option[List[SimpleModule]] = None,
-                               notes: Option[List[SimpleNote]] = None) extends Collaboration {
+                               modules: Option[List[Module]] = None,
+                               notes: Option[List[Note]] = None) extends Collaboration {
 
-}
-
-
-object SimpleCollaboration {
-
-  implicit val simpleCollaborationReads: Reads[SimpleCollaboration] = (
-    (JsPath \ "id").readNullable[String] and
-      (JsPath \ "name").read[String] and
-      (JsPath \ "collaborationType").read[CollaborationType] and
-      (JsPath \ "users").readNullable[List[CollaborationUser]] and
-      (JsPath \ "modules").readNullable[List[SimpleModule]] and
-      (JsPath \ "notes").readNullable[List[SimpleNote]]
-  )(SimpleCollaboration.apply _)
-
-  implicit val simpleCollaborationWrites: Writes[SimpleCollaboration] = (
-    (JsPath \ "id").writeNullable[String] and
-      (JsPath \ "name").write[String] and
-      (JsPath \ "collaborationType").write[CollaborationType] and
-      (JsPath \ "users").writeNullable[List[CollaborationUser]] and
-      (JsPath \ "modules").writeNullable[List[SimpleModule]] and
-      (JsPath \ "notes").writeNullable[List[SimpleNote]]
-  )(unlift(SimpleCollaboration.unapply))
-
-  implicit object BSONtoCollaboration extends BSONDocumentReader[SimpleCollaboration] {
-    def read(doc: BSONDocument): SimpleCollaboration = {
-      SimpleCollaboration(
-        id = doc.getAs[BSONObjectID]("_id").map(id => id.stringify),
-        name = doc.getAs[String]("name").get,
-        collaborationType = doc.getAs[String]("collaborationType").map(t => CollaborationType.withName(t)).get,
-        users = doc.getAs[List[CollaborationUser]]("users"),
-        modules = doc.getAs[List[SimpleModule]]("modules"),
-        notes = doc.getAs[List[SimpleNote]]("notes")
-      )
-    }
-  }
-
-  implicit object CollaborationToBSON extends BSONDocumentWriter[SimpleCollaboration] {
-    def write(collaboration: SimpleCollaboration): BSONDocument = {
-      var newCollaboration = BSONDocument()
-      if (collaboration.id.isDefined) newCollaboration = newCollaboration.merge("_id" -> BSONObjectID.parse(collaboration.id.get).get)
-      else newCollaboration = newCollaboration.merge("_id" -> BSONObjectID.generate())
-      newCollaboration = newCollaboration.merge(BSONDocument("name" -> collaboration.name))
-      newCollaboration = newCollaboration.merge(BSONDocument("collaborationType" -> collaboration.collaborationType.toString))
-
-      if (collaboration.users.isDefined) {
-        val arr = BSONArray(collaboration.users.get.map(e => BSON.write(e)))
-        newCollaboration = newCollaboration.merge(BSONDocument("users" -> arr))
-      }
-      if (collaboration.modules.isDefined) {
-        val arr = BSONArray(collaboration.modules.get)
-        newCollaboration = newCollaboration.merge(BSONDocument("modules" -> arr))
-      }
-      if (collaboration.notes.isDefined) {
-        val arr = BSONArray(collaboration.notes.get)
-        newCollaboration = newCollaboration.merge(BSONDocument("notes" -> arr))
-      }
-
-      newCollaboration
-    }
-  }
 }
 
 object CollectionImplicitTest extends App {
@@ -92,5 +32,5 @@ object CollectionImplicitTest extends App {
                                                       SimpleNote(Option("prova2"),"questo è il contenuto2",Option(new DateTime()),Option(Location(233.32,233.42)),Option.empty,NoteState("done", Option("peru")),Option.empty))))
   val jsn = Json.toJson(collaboration)
   println("Json format: " + jsn)
-  println("Object format: " + jsn.as[SimpleCollaboration])
+  println("Object format: " + jsn.as[Collaboration])
 }

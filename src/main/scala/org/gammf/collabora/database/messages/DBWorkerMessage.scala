fcd654e537f59111f65825dbbd58b009fdde88d4
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-08-22 17:27:34 +0200

    QueryMessage refactored (case classes now work with traits), creation of DBWorkerMessage

--- src/main/scala/org/gammf/collabora/database/messages/DBWorkerMessage.scala
+++ src/main/scala/org/gammf/collabora/database/messages/DBWorkerMessage.scala
@@ -1,7 +1,5 @@
 package org.gammf.collabora.database.messages
 
-import org.gammf.collabora.util.UpdateMessage
-
 /**
   * A simple trait representing a message created from a db worker
   */
@@ -10,4 +8,4 @@ trait DBWorkerMessage
 /**
   * A simple case class representing a message used to tell that a certain operation on the db is succeeded
   */
-case class QueryOkMessage(messageToBeSent: UpdateMessage) extends DBWorkerMessage
+case class QueryOkMessage(queryGoneWell: QueryMessage) extends DBWorkerMessage

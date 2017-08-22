c9866f4e9880d5ece43c18f96fc687d97add1be0
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-08-22 12:57:57 +0200

    finished collaborationMember Actor tests

--- src/test/scala/org/gammf/collabora/communication/actors/CollaborationMembersActorTest.scala
+++ src/test/scala/org/gammf/collabora/communication/actors/CollaborationMembersActorTest.scala
@@ -2,15 +2,22 @@ package org.gammf.collabora.communication.actors
 
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{DefaultTimeout, ImplicitSender, TestKit}
-import com.newmotion.akka.rabbitmq.{ ConnectionActor, ConnectionFactory}
+import com.newmotion.akka.rabbitmq.{ConnectionActor, ConnectionFactory}
+import com.rabbitmq.client._
 import org.gammf.collabora.communication.Utils.CommunicationType
 import org.gammf.collabora.communication.messages._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.{JsObject, JsString, JsValue, Json}
 
 import scala.concurrent.duration._
+import org.scalatest.concurrent.Eventually
 
-class CollaborationMembersActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpecLike with DefaultTimeout with Matchers with BeforeAndAfterAll with ImplicitSender {
+
+class CollaborationMembersActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpecLike with Eventually with DefaultTimeout with Matchers with BeforeAndAfterAll with ImplicitSender {
+
+  private val EXCHANGE_NAME = "collaborations"
+  private val ROUTING_KEY = "maffone"
+  private val BROKER_HOST = "localhost"
 
   val factory = new ConnectionFactory()
   val connection:ActorRef = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
@@ -21,7 +28,7 @@ class CollaborationMembersActorTest extends TestKit (ActorSystem("CollaboraServe
     new CollaborationMembersActor(connection, naming, channelCreator, publisher)), "collaboration-members")
   val subscriber:ActorRef = system.actorOf(Props[SubscriberActor], "subscriber")
 
-
+  var msg: String = ""
   val message : JsValue = Json.parse("""
   {
       "user": "manuelperuzzi",
@@ -51,6 +58,22 @@ class CollaborationMembersActorTest extends TestKit (ActorSystem("CollaboraServe
       }
   }""")
 
+  override def beforeAll(): Unit = {
+      val factory = new ConnectionFactory
+      factory.setHost(BROKER_HOST)
+      val connection = factory.newConnection
+      val channel = connection.createChannel
+      channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT, true)
+      val queueName = channel.queueDeclare.getQueue
+      channel.queueBind(queueName, EXCHANGE_NAME, ROUTING_KEY)
+      val consumer = new DefaultConsumer(channel) {
+        override def handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: Array[Byte]): Unit = {
+          msg = new String(body, "UTF-8")
+          //System.out.println(" [x] Received '" + msg + "'")
+        }
+      }
+      channel.basicConsume(queueName, true, consumer)
+  }
 
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
@@ -76,6 +99,13 @@ class CollaborationMembersActorTest extends TestKit (ActorSystem("CollaboraServe
       collaborationMember ! PublishMemberAddedMessage("maffone", message)
       collaborationMember ! StartMessage
 
+    }
+
+    "check message sended and recived is the same" in {
+      eventually{
+        msg should not be ""
+      }
+      assert(msg.equals(message.toString()))
     }
 
 
--- src/test/scala/org/gammf/collabora/database/actors/DBActorTest.scala
+++ src/test/scala/org/gammf/collabora/database/actors/DBActorTest.scala
@@ -3,6 +3,7 @@ package org.gammf.collabora.database.actors
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import com.newmotion.akka.rabbitmq.{ConnectionActor, ConnectionFactory}
+import com.rabbitmq.client._
 import org.gammf.collabora.communication.actors._
 import org.gammf.collabora.database.messages.{AskConnectionMessage, GetConnectionMessage, InsertNoteMessage}
 import org.gammf.collabora.util.UpdateMessageImpl
@@ -11,6 +12,10 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import scala.concurrent.duration._
 
 class DBActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpecLike  with Matchers with BeforeAndAfterAll with ImplicitSender {
+
+  private val EXCHANGE_NAME = "collaborations"
+  private val ROUTING_KEY = "maffone"
+  private val BROKER_HOST = "localhost"
 
   val dbConnectionActor: ActorRef = system.actorOf(Props[ConnectionManagerActor])
   val factory = new ConnectionFactory()
@@ -24,6 +29,23 @@ class DBActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpec
   val updatesReceiver:ActorRef = system.actorOf(Props(
     new UpdatesReceiverActor(connection, naming, channelCreator, subscriber, dbActor)), "updates-receiver")
 
+
+  override def beforeAll(): Unit = {
+    val factory = new ConnectionFactory
+    factory.setHost(BROKER_HOST)
+    val connection = factory.newConnection
+    val channel = connection.createChannel
+    channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT, true)
+    val queueName = channel.queueDeclare.getQueue
+    channel.queueBind(queueName, EXCHANGE_NAME, ROUTING_KEY)
+    val consumer = new DefaultConsumer(channel) {
+      override def handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: Array[Byte]): Unit = {
+        val message = new String(body, "UTF-8")
+        //System.out.println(" [x] Received '" + message + "'")
+      }
+    }
+    channel.basicConsume(queueName, true, consumer)
+  }
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
   }

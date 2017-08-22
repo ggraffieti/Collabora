f667efda566901304ad524c4a7df739ee33f0a41
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-08-22 19:33:33 +0200

    finish tests on DBWorkerNotes actor

--- src/test/scala/org/gammf/collabora/database/actors/DBWorkerNotesActorTest.scala
+++ src/test/scala/org/gammf/collabora/database/actors/DBWorkerNotesActorTest.scala
@@ -5,24 +5,26 @@ import akka.testkit.{ImplicitSender, TestKit}
 import com.newmotion.akka.rabbitmq.{ConnectionActor, ConnectionFactory}
 import org.gammf.collabora.communication.actors._
 import org.gammf.collabora.communication.messages.{ClientUpdateMessage, StartMessage}
-import org.gammf.collabora.database.messages.{AskConnectionMessage, GetConnectionMessage, QueryOkMessage}
-import org.gammf.collabora.util.UpdateMessage
+import org.gammf.collabora.database.messages._
+import org.gammf.collabora.util.{Location, Note, NoteState, SimpleNote, UpdateMessage}
+import org.joda.time.DateTime
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import play.api.libs.json.{JsError, JsSuccess, Json}
 
+import scala.concurrent.duration._
+
 class DBWorkerNotesActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpecLike  with Matchers with BeforeAndAfterAll with ImplicitSender {
 
-  val dbConnectionActor: ActorRef = system.actorOf(Props[ConnectionManagerActor])
   val factory = new ConnectionFactory()
-  val connection: ActorRef = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
-  val naming: ActorRef = system.actorOf(Props[RabbitMQNamingActor], "naming")
-  val channelCreator: ActorRef = system.actorOf(Props[ChannelCreatorActor], "channelCreator")
-  val publisherActor: ActorRef = system.actorOf(Props[PublisherActor], "publisher")
-  val notificationActor: ActorRef = system.actorOf(Props(new NotificationsSenderActor(connection, naming, channelCreator, publisherActor)))
+  val connection:ActorRef = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
+  val naming:ActorRef = system.actorOf(Props[RabbitMQNamingActor], "naming")
+  val channelCreator :ActorRef= system.actorOf(Props[ChannelCreatorActor], "channelCreator")
+  val publisherActor:ActorRef = system.actorOf(Props[PublisherActor], "publisher")
+  val notificationActor:ActorRef = system.actorOf(Props(new NotificationsSenderActor(connection, naming, channelCreator, publisherActor)))
+  val dbConnectionActor :ActorRef= system.actorOf(Props[ConnectionManagerActor])
   val dbMasterActor:ActorRef = system.actorOf(Props.create(classOf[DBMasterActor], system, notificationActor))
-  val subscriber: ActorRef = system.actorOf(Props[SubscriberActor], "subscriber")
-  val updatesReceiver: ActorRef = system.actorOf(Props(
-    new UpdatesReceiverActor(connection, naming, channelCreator, subscriber, dbMasterActor)), "updates-receiver")
+  val connectionManagerActor: ActorRef =  system.actorOf(Props[ConnectionManagerActor])
+  val notesActor:ActorRef = system.actorOf(Props.create(classOf[DBWorkerNotesActor], connectionManagerActor))
 
 
   override def beforeAll(): Unit = {
@@ -35,29 +37,30 @@ class DBWorkerNotesActorTest extends TestKit (ActorSystem("CollaboraServer")) wi
 
   "A DBWorkerNotes actor" should {
     "insert new notes correctly in the db" in {
-      val message = "{\"messageType\": \"CREATION\",\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\",\"target\" : \"NOTE\",\"user\" : \"maffone\",\"note\": {\"content\" : \"c'ho un nervoso che ti ciacherei la testa\",\"expiration\" : \"2017-08-07T06:01:17.171Z\",\"location\" : { \"latitude\" : 546, \"longitude\" : 324 },\"previousNotes\" : [ \"5980710df27da3fcfe0ac88e\", \"5980710df27da3fcfe0ac88f\" ],\"state\" : { \"definition\" : \"done\", \"username\" : \"maffone\"}}}"
-      updatesReceiver ! StartMessage
-      notificationActor ! StartMessage
-      //updatesReceiver ! ClientUpdateMessage(message)
-      Json.parse(message).validate[UpdateMessage] match {
-        case m: JsSuccess[UpdateMessage] => dbMasterActor ! m.value
-        case error: JsError => println(error)
+      val note:Note = SimpleNote(None, "prova test", None,
+        None, None, new NoteState("done",None), None)
+      within(1 second) {
+        notesActor ! InsertNoteMessage(note, "59806a4af27da3fcfe0ac0ca", "maffone")
+        expectMsgType[QueryOkMessage]
       }
-      //expectMsgType[QueryOkMessage]
     }
 
     "update notes correctly" in {
-      /*val message = "{\"messageType\": \"CREATION\",\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\",\"target\" : \"NOTE\",\"user\" : \"maffone\",\"note\": {\"content\" : \"c'ho un nervoso che ti ciacherei la testa\",\"expiration\" : \"2017-08-07T06:01:17.171Z\",\"location\" : { \"latitude\" : 546, \"longitude\" : 324 },\"previousNotes\" : [ \"5980710df27da3fcfe0ac88e\", \"5980710df27da3fcfe0ac88f\" ],\"state\" : { \"definition\" : \"done\", \"username\" : \"maffone\"}}}"
-      updatesReceiver ! StartMessage
-      updatesReceiver ! ClientUpdateMessage(message)
-      expectMsgType[QueryOkMessage]*/
+      val note:Note = SimpleNote(Option("599c6a1b6700006700e12dc6"), "prova testone ", None,
+        None, None, new NoteState("done",None), None)
+      within(1 second) {
+        notesActor ! UpdateNoteMessage(note, "59806a4af27da3fcfe0ac0ca", "maffone")
+        expectMsgType[QueryOkMessage]
+      }
     }
 
     "delete notes correctly" in {
-      /*val message = "{\"messageType\": \"CREATION\",\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\",\"target\" : \"NOTE\",\"user\" : \"maffone\",\"note\": {\"content\" : \"c'ho un nervoso che ti ciacherei la testa\",\"expiration\" : \"2017-08-07T06:01:17.171Z\",\"location\" : { \"latitude\" : 546, \"longitude\" : 324 },\"previousNotes\" : [ \"5980710df27da3fcfe0ac88e\", \"5980710df27da3fcfe0ac88f\" ],\"state\" : { \"definition\" : \"done\", \"username\" : \"maffone\"}}}"
-      updatesReceiver ! StartMessage
-      updatesReceiver ! ClientUpdateMessage(message)
-      expectMsgType[QueryOkMessage]*/
+      val note:Note = SimpleNote(Option("599c6a1b6700006700e12dc6"), "prova test", None,
+        None, None, new NoteState("done",None), None)
+      within(1 second) {
+        notesActor ! DeleteNoteMessage(note, "59806a4af27da3fcfe0ac0ca", "maffone")
+        expectMsgType[QueryOkMessage]
+      }
     }
 
 

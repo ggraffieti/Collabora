7e4c64fc5a7325c16c9c3a168ed8fa1829f0263a
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-08-22 21:02:41 +0200

    start to test DBWorkerMember actor

--- src/test/scala/org/gammf/collabora/database/actors/DBWorkerMemberActorTest.scala
+++ src/test/scala/org/gammf/collabora/database/actors/DBWorkerMemberActorTest.scala
@@ -4,25 +4,27 @@ import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import com.newmotion.akka.rabbitmq.{ConnectionActor, ConnectionFactory}
 import org.gammf.collabora.communication.actors._
-import org.gammf.collabora.database.messages.{AskConnectionMessage, GetConnectionMessage}
+import org.gammf.collabora.database.messages._
+import org.gammf.collabora.util.{Module, SimpleModule, SimpleUser, User}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.duration._
 
 class DBWorkerMemberActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpecLike  with Matchers with BeforeAndAfterAll with ImplicitSender {
 
-  val dbConnectionActor: ActorRef = system.actorOf(Props[ConnectionManagerActor])
   val factory = new ConnectionFactory()
-  val connection: ActorRef = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
-  val naming: ActorRef = system.actorOf(Props[RabbitMQNamingActor], "naming")
-  val channelCreator: ActorRef = system.actorOf(Props[ChannelCreatorActor], "channelCreator")
-  val publisherActor: ActorRef = system.actorOf(Props[PublisherActor], "publisher")
-  val notificationActor: ActorRef = system.actorOf(Props(new NotificationsSenderActor(connection, naming, channelCreator, publisherActor)))
+  val connection:ActorRef = system.actorOf(ConnectionActor.props(factory), "rabbitmq")
+  val naming:ActorRef = system.actorOf(Props[RabbitMQNamingActor], "naming")
+  val channelCreator :ActorRef= system.actorOf(Props[ChannelCreatorActor], "channelCreator")
+  val publisherActor:ActorRef = system.actorOf(Props[PublisherActor], "publisher")
+  val notificationActor:ActorRef = system.actorOf(Props(new NotificationsSenderActor(connection, naming, channelCreator, publisherActor)))
+  val dbConnectionActor :ActorRef= system.actorOf(Props[ConnectionManagerActor])
   val dbMasterActor:ActorRef = system.actorOf(Props.create(classOf[DBMasterActor], system, notificationActor))
-  val subscriber: ActorRef = system.actorOf(Props[SubscriberActor], "subscriber")
-  val updatesReceiver: ActorRef = system.actorOf(Props(
-    new UpdatesReceiverActor(connection, naming, channelCreator, subscriber, dbMasterActor)), "updates-receiver")
+  val connectionManagerActor: ActorRef =  system.actorOf(Props[ConnectionManagerActor])
+  val usersActor:ActorRef = system.actorOf(Props.create(classOf[DBWorkerMemberActor], connectionManagerActor))
+  val userID:String = "123456788000000004567700"
 
+  val user:User = SimpleUser(Option(userID),"questo è un modulo importante",None,"doing")
 
   override def beforeAll(): Unit = {
 
@@ -32,18 +34,30 @@ class DBWorkerMemberActorTest extends TestKit (ActorSystem("CollaboraServer")) w
     TestKit.shutdownActorSystem(system)
   }
 
-  "A DB actor" should {
-    "send back connection message correctly" in {
-      within(500 millis) {
-        dbConnectionActor ! new AskConnectionMessage()
-        expectMsgType[GetConnectionMessage]
+  "A DBWorkerMember actor" should {
+    "insert new modules in a collaboration correctly in the db" in {
+      within(3 second) {
+        usersActor ! InsertUserMessage(user, "59806a4af27da3fcfe0ac0ca", "maffone")
+        expectMsgType[QueryOkMessage]
       }
     }
 
-    "insert note in the db and after that, send a publish to NotificationActor" in {
-      within(500 millis) {
+    "update a module in a collaboration correctly" in {
+      within(1 second) {
+        usersActor ! UpdateUserMessage(user, "59806a4af27da3fcfe0ac0ca", "maffone")
+        expectMsgType[QueryOkMessage]
       }
     }
+
+    "delete a module in a collaboration correctly" in {
+      within(1 second) {
+        usersActor ! DeleteUserMessage(user, "59806a4af27da3fcfe0ac0ca", "maffone")
+        expectMsgType[QueryOkMessage]
+      }
+    }
+
+
+
   }
 }
 

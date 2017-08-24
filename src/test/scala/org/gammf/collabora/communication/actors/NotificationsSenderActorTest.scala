9871a44dffc335cda40ec403471eed057748d42f
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-08-24 17:41:36 +0200

    Little fix in note json format

--- src/test/scala/org/gammf/collabora/communication/actors/NotificationsSenderActorTest.scala
+++ src/test/scala/org/gammf/collabora/communication/actors/NotificationsSenderActorTest.scala
@@ -77,7 +77,7 @@ class NotificationsSenderActorTest extends TestKit (ActorSystem("CollaboraServer
     }
 
     "notify clients when there are updates on db" in {
-      val message = "{\"messageType\": \"CREATION\",\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\",\"target\" : \"NOTE\",\"user\" : \"maffone\",\"note\": {\"content\" : \"c'ho un nervoso che ti ciacherei la testa\",\"expiration\" : \"2017-08-07T06:01:17.171Z\",\"location\" : { \"latitude\" : 546, \"longitude\" : 324 },\"previousNotes\" : [ \"5980710df27da3fcfe0ac88e\", \"5980710df27da3fcfe0ac88f\" ],\"state\" : { \"definition\" : \"done\", \"username\" : \"maffone\"}}}"
+      val message = "{\"messageType\": \"CREATION\",\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\",\"target\" : \"NOTE\",\"user\" : \"maffone\",\"note\": {\"content\" : \"c'ho un nervoso che ti ciacherei la testa\",\"expiration\" : \"2017-08-07T06:01:17.171Z\",\"location\" : { \"latitude\" : 546, \"longitude\" : 324 },\"previousNotes\" : [ \"5980710df27da3fcfe0ac88e\", \"5980710df27da3fcfe0ac88f\" ],\"state\" : { \"definition\" : \"done\", \"responsible\" : \"maffone\"}}}"
       updatesReceiver ! StartMessage
       notificationActor ! StartMessage
       updatesReceiver ! ClientUpdateMessage(message)
@@ -85,7 +85,8 @@ class NotificationsSenderActorTest extends TestKit (ActorSystem("CollaboraServer
         msg should not be ""
       }
       val startMsg = "{\"target\":\"NOTE\",\"messageType\":\"CREATION\",\"user\":\"maffone\",\"note\":{\"id\":"
-      val endMsg = "\"content\":\"c'ho un nervoso che ti ciacherei la testa\",\"expiration\":\"2017-08-07T08:01:17.171+02:00\",\"location\":{\"latitude\":546,\"longitude\":324},\"previousNotes\":[\"5980710df27da3fcfe0ac88e\",\"5980710df27da3fcfe0ac88f\"],\"state\":{\"definition\":\"done\",\"username\":\"maffone\"}},\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\"}"
+      val endMsg = "\"content\":\"c'ho un nervoso che ti ciacherei la testa\",\"expiration\":\"2017-08-07T08:01:17.171+02:00\",\"location\":{\"latitude\":546,\"longitude\":324},\"previousNotes\":[\"5980710df27da3fcfe0ac88e\",\"5980710df27da3fcfe0ac88f\"],\"state\":{\"definition\":\"done\",\"responsible\":\"maffone\"}},\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\"}"
+
       assert(msg.startsWith(startMsg)&& msg.endsWith(endMsg))
     }
 

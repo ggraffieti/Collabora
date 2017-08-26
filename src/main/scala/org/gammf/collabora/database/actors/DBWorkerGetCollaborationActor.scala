fe1777139059da3c17244e5c281f106a60903cfb
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-08-26 10:20:41 +0200

    added JavaDoc

--- src/main/scala/org/gammf/collabora/database/actors/DBWorkerGetCollaborationActor.scala
+++ src/main/scala/org/gammf/collabora/database/actors/DBWorkerGetCollaborationActor.scala
@@ -4,12 +4,16 @@ import akka.actor.{ActorRef, Stash}
 import org.gammf.collabora.communication.messages.PublishMemberAddedMessage
 import org.gammf.collabora.database.messages._
 import org.gammf.collabora.util.{Collaboration, CollaborationMessage}
-import play.api.libs.json.Json
 import reactivemongo.bson.{BSONDocument, BSONObjectID}
 
 import scala.util.{Failure, Success}
 import scala.concurrent.ExecutionContext.Implicits.global
 
+/***
+  * A worker that perform a get on collaboration collection and communicate with CollaborationMembersActor
+  * @param connectionActor the actor that mantains the connection with the DB.
+  * @param collaborationActor the actor that manage the exchange "collaborations"
+  */
 class DBWorkerGetCollaborationActor(connectionActor: ActorRef, collaborationActor: ActorRef) extends DBWorker(connectionActor) with Stash {
   override def receive: Receive = {
     case m: GetConnectionMessage =>

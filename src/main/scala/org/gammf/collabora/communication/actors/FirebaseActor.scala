119bd308dc0e0e6b4cea7c397487b7aaf6cfc0cb
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-08-29 11:50:06 +0200

    added JavaDoc on FirebaseActor

--- src/main/scala/org/gammf/collabora/communication/actors/FirebaseActor.scala
+++ src/main/scala/org/gammf/collabora/communication/actors/FirebaseActor.scala
@@ -7,12 +7,17 @@ import org.gammf.collabora.database.actors.{ConnectionManagerActor, DBWorkerGetC
 import org.gammf.collabora.database.messages.GetCollaboration
 import org.gammf.collabora.util.{Firebase, UpdateMessage, UpdateMessageTarget, UpdateMessageType}
 
+/**
+  * This is an actor that sends Firebase notification about Note,Module,Member operation
+  * to all the client that are registered in the specific collaborationID Topic
+  * @param collaborationGetter The actor who will be asked to take the collaboration from the DB
+  */
 class FirebaseActor(collaborationGetter: ActorRef) extends Actor{
 
   private val AUTHORIZATION = "AAAAJtSw2Gk:APA91bEXmB5sRFqSnuYIP3qofHQ0RfHrAzTllJ0vYWtHXKZsMdbuXmUKbr16BVZsMO0cMmm_BWE8oLzkFcyuMr_V6O6ilqvLu7TrOgirVES51Ux9PsKfJ17iOMvTF_WtwqEURqMGBbLf"
   private[this] var info: Option[UpdateMessage] = None
   private[this] val firebase: Firebase = new Firebase
-  
+
   override def receive:Receive = {
     case PublishNotificationMessage(collaborationID, message) =>
       firebase.setKey(AUTHORIZATION)
@@ -20,13 +25,17 @@ class FirebaseActor(collaborationGetter: ActorRef) extends Actor{
       collaborationGetter ! GetCollaboration(collaborationID)
     case PublishFirebaseNotification(collaborationID,collaboration)=>
       firebase.setTtile(collaboration.name)
-      firebase.setBody(setTextType()+setTextTarget())
+      firebase.setBody(setUserAndOperation()+setTextTarget())
       firebase.to(collaborationID)
       firebase.send()
     case _ => println("[FirebaseActor] Huh?")
   }
 
-  private def setTextType(): String = {
+  /**
+    * Method used to insert in the message the User and the operation that the user requested
+    * @return
+    */
+  private def setUserAndOperation(): String = {
     val notifyText: String = info.get.user
       info.get.messageType match {
         case UpdateMessageType.CREATION => notifyText + " added"
@@ -35,6 +44,10 @@ class FirebaseActor(collaborationGetter: ActorRef) extends Actor{
       }
   }
 
+  /**
+    * Method used to insert in the message the object of the requested operation
+    * @return
+    */
   private def setTextTarget(): String = {
     info.get.target match {
       case UpdateMessageTarget.NOTE => " a note"

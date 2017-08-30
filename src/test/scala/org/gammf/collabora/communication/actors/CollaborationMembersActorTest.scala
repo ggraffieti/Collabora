428e399c2372102c2ab2189b37c6baf2aa282172
Federico Vitali <federico.vitali2@studio.unibo.it>
2017-08-30 14:22:40 +0200

    changed DB structure and refactor it

--- src/test/scala/org/gammf/collabora/communication/actors/CollaborationMembersActorTest.scala
+++ src/test/scala/org/gammf/collabora/communication/actors/CollaborationMembersActorTest.scala
@@ -33,7 +33,7 @@ class CollaborationMembersActorTest extends TestKit (ActorSystem("CollaboraServe
 
   override def beforeAll(): Unit = {
       fakeReceiver("collaborations","maffone","localhost")
-      fakeReceiver("notifications","123456788698540008900400","localhost")
+      fakeReceiver("notifications","59804868f27da3fcfe0a8e20","localhost")
   }
 
   override def afterAll(): Unit = {
@@ -62,7 +62,7 @@ class CollaborationMembersActorTest extends TestKit (ActorSystem("CollaboraServe
     }
 
     "send collaboration to user that have just added and a notification to all the old member of collaboration" in {
-      val message = "{\"messageType\": \"CREATION\",\"target\" : \"MEMBER\",\"user\" : \"maffone\",\"member\": {\"user\": \"maffone\",\"right\": \"WRITE\"},\"collaborationId\":\"123456788698540008900400\"}"
+      val message = "{\"messageType\": \"CREATION\",\"target\" : \"MEMBER\",\"user\" : \"maffone\",\"member\": {\"user\": \"maffone\",\"right\": \"WRITE\"},\"collaborationId\":\"59804868f27da3fcfe0a8e20\"}"
       notificationActor ! StartMessage
       collaborationMember ! StartMessage
       updatesReceiver ! StartMessage
@@ -74,7 +74,7 @@ class CollaborationMembersActorTest extends TestKit (ActorSystem("CollaboraServe
       System.out.println(msgCollab)
       System.out.println(msgNotif)
       assert(msgNotif.startsWith("{\"target\":\"MEMBER\",\"messageType\":\"CREATION\",\"user\":\"maffone\",\"member\"")
-            && msgCollab.startsWith("{\"user\":\"maffone\",\"collaboration\":{\"id\":\"123456788698540008900400\",\"name\":\"simplecollaboration\",\"collaborationType\":\"GROUP\""))
+            && msgCollab.startsWith("{\"user\":\"maffone\",\"collaboration\":{\"id\":\"59804868f27da3fcfe0a8e20\",\"name\":\"Prova Project\",\"collaborationType\":\"GROUP\""))
     }
 
 
--- src/test/scala/org/gammf/collabora/communication/actors/NotificationsSenderActorTest.scala
+++ src/test/scala/org/gammf/collabora/communication/actors/NotificationsSenderActorTest.scala
@@ -80,7 +80,7 @@ class NotificationsSenderActorTest extends TestKit (ActorSystem("CollaboraServer
     }
 
     "notify clients when there are updates on db" in {
-      val message = "{\"messageType\": \"CREATION\",\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\",\"target\" : \"NOTE\",\"user\" : \"maffone\",\"note\": {\"content\" : \"c'ho un nervoso che ti ciacherei la testa\",\"expiration\" : \"2017-08-07T08:01:17.171+02:00\",\"location\" : { \"latitude\" : 546, \"longitude\" : 324 },\"previousNotes\" : [ \"5980710df27da3fcfe0ac88e\", \"5980710df27da3fcfe0ac88f\" ],\"state\" : { \"definition\" : \"done\", \"responsible\" : \"maffone\"}}}"
+      val message = "{\"messageType\": \"CREATION\",\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\",\"target\" : \"NOTE\",\"user\" : \"maffone\",\"note\": {\"content\" : \"prova test\",\"expiration\" : \"2017-08-07T08:01:17.171+02:00\",\"location\" : { \"latitude\" : 546, \"longitude\" : 324 },\"previousNotes\" : [ \"5980710df27da3fcfe0ac88e\", \"5980710df27da3fcfe0ac88f\" ],\"state\" : { \"definition\" : \"done\", \"responsible\" : \"maffone\"}}}"
       updatesReceiver ! StartMessage
       notificationActor ! StartMessage
       updatesReceiver ! ClientUpdateMessage(message)
@@ -88,7 +88,7 @@ class NotificationsSenderActorTest extends TestKit (ActorSystem("CollaboraServer
         msg should not be ""
       }
       val startMsg = "{\"target\":\"NOTE\",\"messageType\":\"CREATION\",\"user\":\"maffone\",\"note\":{\"id\":"
-      val endMsg = "\"content\":\"c'ho un nervoso che ti ciacherei la testa\",\"expiration\":\"2017-08-07T08:01:17.171+02:00\",\"location\":{\"latitude\":546,\"longitude\":324},\"previousNotes\":[\"5980710df27da3fcfe0ac88e\",\"5980710df27da3fcfe0ac88f\"],\"state\":{\"definition\":\"done\",\"responsible\":\"maffone\"}},\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\"}"
+      val endMsg = "\"content\":\"prova test\",\"expiration\":\"2017-08-07T08:01:17.171+02:00\",\"location\":{\"latitude\":546,\"longitude\":324},\"previousNotes\":[\"5980710df27da3fcfe0ac88e\",\"5980710df27da3fcfe0ac88f\"],\"state\":{\"definition\":\"done\",\"responsible\":\"maffone\"}},\"collaborationId\":\"59806a4af27da3fcfe0ac0ca\"}"
 
       assert(msg.startsWith(startMsg)&& msg.endsWith(endMsg))
     }

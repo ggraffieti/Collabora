a81f266ca317221872eda0062bbad60c4b456888
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-09-02 20:13:50 +0200

    Added yellow pages actor request and response messages

--- src/main/scala/org/gammf/collabora/yellowpages/messages/YellowPagesMessage.scala
+++ src/main/scala/org/gammf/collabora/yellowpages/messages/YellowPagesMessage.scala
@@ -1,5 +1,6 @@
 package org.gammf.collabora.yellowpages.messages
 
+import akka.actor.ActorRef
 import org.gammf.collabora.yellowpages.ActorService.ActorService
 import org.gammf.collabora.yellowpages.TopicElement.TopicElement
 import org.gammf.collabora.yellowpages.util.Topic
@@ -10,19 +11,48 @@ import org.gammf.collabora.yellowpages.util.Topic
 trait YellowPagesMessage
 
 /**
-  * Represents a registration request from an actor.
-  * Contains the topic to which the actor asks to be registered and the service that offers.
+  * Represents a registration request from an actor that wants to be included in the yellow pages.
   * @param topic the topic to which the actor asks to be registered.
   * @param service the service offered by the actor.
   */
-case class RegistrationMessage(topic: Topic[TopicElement], service: ActorService) extends YellowPagesMessage
+case class RegistrationRequestMessage(topic: Topic[TopicElement], service: ActorService)
+  extends YellowPagesMessage
 
 /**
   * Represents a confirmation of actor registration in the yellow pages.
   */
-case class RegistrationOKMessage() extends YellowPagesMessage
+case class RegistrationOKMessage()
+  extends YellowPagesMessage
 
 /**
   * Represents an error in the actor registration in the yellow pages.
   */
-case class RegistrationErrorMessage() extends YellowPagesMessage
+case class RegistrationErrorMessage()
+  extends YellowPagesMessage
+
+/**
+  * Represents an actor request from an actor, that asks for the reference of an actor with the specified topic and service.
+  * @param topic the topic to which the requested actor have to be registered.
+  * @param service the service offered by the requested actor.
+  */
+case class ActorRequestMessage(topic: Topic[TopicElement], service: ActorService)
+  extends YellowPagesMessage
+
+/**
+  * Represents a positive response to an actor request message.
+  * Contains a reference to an actor that meets all the requested specifications.
+  * @param actor the reference to an actor with the specified topic and service.
+  * @param topic the requested actor topic.
+  * @param service the requested actor service.
+  */
+case class ActorOKMessage(actor: ActorRef, topic: Topic[TopicElement], service: ActorService)
+  extends YellowPagesMessage
+
+/**
+  * Represents a negative response to an actor request message.
+  * Should be used when an actor with the specified topic and service was not found in the yellow pages.
+  * @param topic the topic requested in the actor request.
+  * @param service the service requested in the actor request.
+  */
+case class ActorErrorMessage(topic: Topic[TopicElement], service: ActorService)
+  extends YellowPagesMessage

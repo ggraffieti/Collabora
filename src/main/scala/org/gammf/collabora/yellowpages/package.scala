5d451bd5664964258850579ba4fbe5c741049cc4
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-09-02 17:08:36 +0200

    Added type and updates some names in actor service enumeration and topic element enumeration

--- src/main/scala/org/gammf/collabora/yellowpages/package.scala
+++ src/main/scala/org/gammf/collabora/yellowpages/package.scala
@@ -5,10 +5,12 @@ package object yellowpages {
     * An enumeration containing all the application related topics.
     */
   object TopicElement extends Enumeration {
-    val communication,
-    rabbitmq,
-    firebase,
-    http = Value
+    type TopicElement = Value
+
+    val Communication,
+        Rabbitmq,
+        Firebase,
+        Http = Value
 
     val database = Value
     //TODO list all the database related topics
@@ -18,18 +20,20 @@ package object yellowpages {
     * An enumeration containing all the application related servicies that an actor can offer.
     */
   object ActorService extends Enumeration {
+    type ActorService = Value
+
     // Yellow pages related services
     val yellowPagesService = Value
 
     // Communication related services
     // TODO update the list with http related services
-    val channelCreating,
-    naming,
-    publishing,
-    subscribing,
-    notificationSending,
-    collaborationSending,
-    updatesReceiving = Value
+    val ChannelCreating,
+        Naming,
+        Publishing,
+        Subscribing,
+        NotificationSending,
+        CollaborationSending,
+        UpdatesReceiving = Value
 
     // Database related services
     // TODO list all the database related services

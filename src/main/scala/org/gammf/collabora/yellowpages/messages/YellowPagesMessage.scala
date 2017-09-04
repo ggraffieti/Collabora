268116057efa5ea1baaf1a0c72a0444e74ef4762
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-09-05 00:14:31 +0200

    Added some yellow pages messages

--- src/main/scala/org/gammf/collabora/yellowpages/messages/YellowPagesMessage.scala
+++ src/main/scala/org/gammf/collabora/yellowpages/messages/YellowPagesMessage.scala
@@ -10,6 +10,12 @@ import org.gammf.collabora.yellowpages.util.Topic
   */
 trait YellowPagesMessage
 
+trait InsertionRequestMessage extends YellowPagesMessage {
+  def actor: ActorRef
+  def topic: Topic[TopicElement]
+  def service: ActorService
+}
+
 /**
   * Represents a registration request from an actor that wants to be included in the yellow pages.
   * @param actor the actor that sends the registration request.
@@ -17,18 +23,52 @@ trait YellowPagesMessage
   * @param service the service offered by the actor.
   */
 case class RegistrationRequestMessage(actor: ActorRef, topic: Topic[TopicElement], service: ActorService)
+  extends InsertionRequestMessage
+
+/**
+  * Represents a message between objects of type [[org.gammf.collabora.yellowpages.actors.YellowPagesActor]] containing the info about an entry.
+  * An actor should send this message to delegate an entry management to another actor.
+  * @param actor the actor of the entry.
+  * @param topic the topic of the entry.
+  * @param service the service of the entry.
+  */
+case class RedirectionRequestMessage(actor: ActorRef, topic: Topic[TopicElement], service: ActorService)
+  extends InsertionRequestMessage
+
+/**
+  * Represents a request about a deletion of a yellow pages entries.
+  * @param actor the actor to be unregistered.
+  * @param topic the actor topic.
+  * @param service the actor service.
+  */
+case class UnregistrationRequestMessage(actor: ActorRef, topic: Topic[TopicElement], service: ActorService)
+  extends YellowPagesMessage
+
+/**
+  * Represents a positive response to an [[RedirectionRequestMessage]].
+ *
+  * @param actor the actor of the entry.
+  * @param topic the topic of the entry.
+  * @param service the service of the entry.
+  */
+case class ActorRedirectionOKMessage(actor: ActorRef, topic: Topic[TopicElement], service: ActorService)
+  extends YellowPagesMessage
+
+/**
+  * Represents a negative response to an [[RedirectionRequestMessage]].
+ *
+  * @param actor the actor of the entry.
+  * @param topic the topic of the entry.
+  * @param service the service of the entry.
+  */
+case class ActorRedirectionErrorMessage(actor: ActorRef, topic: Topic[TopicElement], service: ActorService)
   extends YellowPagesMessage
 
 /**
   * Represents a confirmation of actor registration in the yellow pages.
+  * It's a positive response to a [[RegistrationRequestMessage]].
   */
 case class RegistrationOKMessage()
-  extends YellowPagesMessage
-
-/**
-  * Represents an error in the actor registration in the yellow pages.
-  */
-case class RegistrationErrorMessage()
   extends YellowPagesMessage
 
 /**
@@ -40,7 +80,7 @@ case class ActorRequestMessage(topic: Topic[TopicElement], service: ActorService
   extends YellowPagesMessage
 
 /**
-  * Represents a positive response to an actor request message.
+  * Represents a positive response to an [[ActorRequestMessage]].
   * Contains a reference to an actor that meets all the requested specifications.
   * @param actor the reference to an actor with the specified topic and service.
   * @param topic the requested actor topic.
@@ -50,10 +90,15 @@ case class ActorOKMessage(actor: ActorRef, topic: Topic[TopicElement], service: 
   extends YellowPagesMessage
 
 /**
-  * Represents a negative response to an actor request message.
+  * Represents an error in the actor registration in the yellow pages.
+  * It's a negative response to a [[RegistrationRequestMessage]].
+  */
+case class RegistrationErrorMessage() extends YellowPagesMessage
+
+/**
+  * Represents a negative response to an [[ActorRequestMessage]].
   * Should be used when an actor with the specified topic and service was not found in the yellow pages.
   * @param topic the topic requested in the actor request.
   * @param service the service requested in the actor request.
   */
-case class ActorErrorMessage(topic: Topic[TopicElement], service: ActorService)
-  extends YellowPagesMessage
+case class ActorErrorMessage(topic: Topic[TopicElement], service: ActorService) extends YellowPagesMessage
--- src/main/scala/org/gammf/collabora/yellowpages/util/YellowPagesEntry.scala
+++ src/main/scala/org/gammf/collabora/yellowpages/util/YellowPagesEntry.scala
@@ -1,8 +1,10 @@
 package org.gammf.collabora.yellowpages.util
 
 import akka.actor.ActorRef
-import org.gammf.collabora.yellowpages.ActorService.ActorService
-import org.gammf.collabora.yellowpages.TopicElement.TopicElement
+import org.gammf.collabora.yellowpages.ActorService._
+import org.gammf.collabora.yellowpages.TopicElement._
+
+import language.reflectiveCalls
 
 /**
   * Represents a generic yellow pages entry.
@@ -44,7 +46,41 @@ sealed trait YellowPagesEntry[A, B, C] {
   * Represents a yellow pages entry in the actor world.
   * Contains the actor reference, the topic to which the actor is registered and the service offered by the actor.
   */
-sealed trait ActorYellowPagesEntry extends YellowPagesEntry[ActorRef, TopicElement, ActorService]
+sealed trait ActorYellowPagesEntry extends YellowPagesEntry[ActorRef, TopicElement, ActorService] {
+  /**
+    * Similarity method. Compares this entry to some other entry.
+    * @param that the other entry to be compared to this entry.
+    * @return true if the given entry is similar to this entry, false otherwise.
+    */
+  def ===(that: { def topic: Topic[TopicElement]; def service: ActorService }): Boolean
+
+  /**
+    * Greater method. Checks if this entry is greater than another object, based on its topic value.
+    * @param that the object to be compared to this entry.
+    * @return true if this entry is greater than the given object, false otherwise.
+    */
+  def >(that: { def topic: Topic[TopicElement]; def service: ActorService }): Boolean
+  /**
+    * Greater or similar method. Checks if this entry is similar or greater than another object, based on its topic value.
+    * @param that the object to be compared to this entry.
+    * @return true if this entry is similar or greater than the given object, false otherwise.
+    */
+  def >=(that: { def topic: Topic[TopicElement]; def service: ActorService }): Boolean
+
+  /**
+    * Lesser method. Checks if this entry is lesser than another object, based on its topic value.
+    * @param that the object to be compared to this entry.
+    * @return true if this entry is lesser than the given object, false otherwise.
+    */
+  def <(that: { def topic: Topic[TopicElement]; def service: ActorService }): Boolean
+
+  /**
+    * Lesser or similar method. Checks if this entry is similar or lesser than another object, based on its topic value.
+    * @param that the object to be compared to this entry.
+    * @return true if this entry is similar or lesser than the given object, false otherwise.
+    */
+  def <=(that: { def topic: Topic[TopicElement]; def service: ActorService }): Boolean
+}
 
 /**
   * Simple implementation of a yellow pages entry in the actor world.
@@ -55,7 +91,21 @@ sealed trait ActorYellowPagesEntry extends YellowPagesEntry[ActorRef, TopicEleme
   */
 case class ActorYellowPagesEntryImpl(override val reference: ActorRef, override val topic: Topic[TopicElement],
                                      override val service: ActorService, override var used: Boolean = false)
-  extends ActorYellowPagesEntry
+  extends ActorYellowPagesEntry {
+  override def equals(obj: Any): Boolean = obj match {
+    case e: ActorYellowPagesEntry => e.reference == reference && e.topic == topic && e.service == service
+  }
+  override def ===(that: { def service: ActorService; def topic: Topic[TopicElement]}): Boolean =
+    topic == that.topic && service == that.service
+  override def >(that: { def service: ActorService; def topic: Topic[TopicElement]}): Boolean =
+    (that.service == YellowPagesService && topic > that.topic) || (that.service != YellowPagesService && topic >= that.topic)
+  override def >=(that: { def service: ActorService; def topic: Topic[TopicElement]}): Boolean =
+    this > that || this === that
+  override def <(that: { def service: ActorService; def topic: Topic[TopicElement]}): Boolean =
+    (that.service == YellowPagesService && topic < that.topic) || (that.service != YellowPagesService && topic <= that.topic)
+  override def <=(that: { def service: ActorService; def topic: Topic[TopicElement]}): Boolean =
+    this < that || this === that
+}
 
 object ActorYellowPagesEntry {
   /**

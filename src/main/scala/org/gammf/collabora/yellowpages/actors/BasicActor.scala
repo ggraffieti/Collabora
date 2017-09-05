e45673850a1efeb7b1ae7daff60a6eb25f9aa9a1
Manuel Peruzzi <manuel.peruzzi@studio.unibo.it>
2017-09-05 16:52:33 +0200

    Added scaladoc to basic actor

--- src/main/scala/org/gammf/collabora/yellowpages/actors/BasicActor.scala
+++ src/main/scala/org/gammf/collabora/yellowpages/actors/BasicActor.scala
@@ -6,10 +6,29 @@ import org.gammf.collabora.yellowpages.TopicElement.TopicElement
 import org.gammf.collabora.yellowpages.messages._
 import org.gammf.collabora.yellowpages.util.Topic
 
+/**
+  * Represents a simple actor identified by a name, registered to a [[Topic]], offering an [[ActorService]].
+  * In order to communicate with other actors, this actor needs a reference to a [[YellowPagesActor]].
+  */
 trait BasicActor extends Actor {
+  /**
+    * Returns a reference to the yellow pages root actor.
+    */
   def yellowPages: ActorRef
+
+  /**
+    * Returns the name of this actor.
+    */
   def name: String
+
+  /**
+    * Returns the topic to which this actor is registered.
+    */
   def topic: Topic[TopicElement]
+
+  /**
+    * Returns the service offered by this actor.
+    */
   def service: ActorService
 
   override def preStart(): Unit = super.preStart; yellowPages ! RegistrationRequestMessage(self, topic, service)

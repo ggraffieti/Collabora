e1d189e44a1f186a9c3e14cc411129bfc6878ed1
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2017-09-07 21:22:10 +0200

    add users collection constants in database package object, and use them in the code

--- src/main/scala/org/gammf/collabora/database/actors/worker/DBWorkerAuthenticationActor.scala
+++ src/main/scala/org/gammf/collabora/database/actors/worker/DBWorkerAuthenticationActor.scala
@@ -7,8 +7,14 @@ import org.gammf.collabora.database.messages._
 import org.gammf.collabora.util.User
 import reactivemongo.bson.{BSON, BSONDocument}
 
+import org.gammf.collabora.database._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
+/**
+  * A [[DBWorker]] used for authentication purpose. It manages [[LoginMessage]] and [[SigninMessage]].
+  * @param connectionManager the manager of the connection, needed to mantain a stable connection with the database.
+  */
 class DBWorkerAuthenticationActor(connectionManager: ActorRef) extends UsersDBWorker(connectionManager) with Stash {
 
 
@@ -21,7 +27,7 @@ class DBWorkerAuthenticationActor(connectionManager: ActorRef) extends UsersDBWo
 
     case message: LoginMessage =>
       find(
-        selector = BSONDocument("_id" -> message.username),
+        selector = BSONDocument(USER_ID -> message.username),
         okStrategy = bsonDocument =>  {
           if (bsonDocument.isDefined) AuthenticationMessage(Some(bsonDocument.get.as[User]))
           else AuthenticationMessage(None)

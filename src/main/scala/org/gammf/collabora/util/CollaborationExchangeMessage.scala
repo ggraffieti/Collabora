a0f68ca3e8894070a46ab1ad5e414a37784352ff
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2017-09-09 22:59:37 +0200

    deleted class due to json conversion error with class hierarchy

--- src/main/scala/org/gammf/collabora/util/CollaborationExchangeMessage.scala
+++
@@ -1,8 +0,0 @@
-package org.gammf.collabora.util
-
-/**
-  * This trait represents a generic message that have to be sent in the collaboration exchange.
-  */
-trait CollaborationExchangeMessage {
- def user: String
-}

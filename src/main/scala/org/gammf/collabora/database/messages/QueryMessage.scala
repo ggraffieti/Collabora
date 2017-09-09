a5fd25e72286703cc9aa1feb1e08349ed03841d0
Gabriele Graffieti <gabriele.graffieti@gmail.com>
2017-09-09 16:33:57 +0200

    edit actors to handle error message (not already implemented) if a user insert a member not registered in the system

--- src/main/scala/org/gammf/collabora/database/messages/QueryMessage.scala
+++ src/main/scala/org/gammf/collabora/database/messages/QueryMessage.scala
@@ -83,3 +83,18 @@ case class GetCollaboration(collaborationID: String) extends  QueryRetriveCollab
   */
 case class GetAllCollaborationsMessage(username: String) extends QueryMessage
 
+/**
+  * A message that have to be sent to the [[org.gammf.collabora.database.actors.worker.DBWorkerCheckMemberExistenceActor]]
+  * to check the existence of a member in the application.
+  * @param username the username of the member.
+  */
+case class IsMemberExistsMessage(username: String) extends QueryMessage
+
+/**
+  * A message that represents a response to a [[IsMemberExistsMessage]], containing a boolean value that indicates
+  * if the member is registered in the system or no.
+  * @param username the username of the member.
+  * @param isRegistered true if the member is Registered, false otherwise.
+  */
+case class IsMemberExistsResponseMessage(username: String, isRegistered: Boolean) extends QueryMessage
+

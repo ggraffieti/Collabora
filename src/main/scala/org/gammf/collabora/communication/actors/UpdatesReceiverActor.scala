7a471a7e021787d9fa262ead6cff83e71d78b763
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-09-11 16:29:29 +0200

    UpdatesReceiverActor integration with yellowpages

--- src/main/scala/org/gammf/collabora/communication/actors/UpdatesReceiverActor.scala
+++ src/main/scala/org/gammf/collabora/communication/actors/UpdatesReceiverActor.scala
@@ -3,9 +3,17 @@ package org.gammf.collabora.communication.actors
 import akka.actor._
 import org.gammf.collabora.communication.Utils.CommunicationType
 import org.gammf.collabora.communication.messages._
-import org.gammf.collabora.database.messages.InsertNoteMessage
-import org.gammf.collabora.util.{UpdateMessage, UpdateMessageImpl}
+import org.gammf.collabora.util.UpdateMessage
+import org.gammf.collabora.yellowpages.ActorService.ActorService
+import org.gammf.collabora.yellowpages.actors.{BasicActor, YellowPagesActor}
 import play.api.libs.json.{JsError, JsSuccess, Json}
+import org.gammf.collabora.yellowpages.messages._
+import org.gammf.collabora.yellowpages.util.Topic._
+import org.gammf.collabora.yellowpages.util.Topic
+import org.gammf.collabora.yellowpages.TopicElement._
+import org.gammf.collabora.yellowpages.ActorService._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
   * @author Manuel Peruzzi
@@ -18,22 +26,38 @@ import play.api.libs.json.{JsError, JsSuccess, Json}
   * @param channelCreator the reference to a channel creator actor.
   * @param subscriber the reference to a subscriber actor.
   */
-class UpdatesReceiverActor(connection: ActorRef, naming: ActorRef, channelCreator: ActorRef,
-                           subscriber: ActorRef, dbActor: ActorRef) extends Actor {
+class UpdatesReceiverActor(override val yellowPages: ActorRef, override val name: String,
+                           override val topic: ActorTopic, override val service: ActorService) extends BasicActor {
 
   private[this] var subQueue: Option[String] = None
 
-  override def receive: Receive = {
-    case StartMessage => naming ! ChannelNamesRequestMessage(CommunicationType.UPDATES)
-    case ChannelNamesResponseMessage(exchange, queue) =>
-      subQueue = queue
-      channelCreator ! SubscribingChannelCreationMessage(connection, exchange, subQueue.get, None)
-    case ChannelCreatedMessage(channel) => subscriber ! SubscribeMessage(channel, subQueue.get)
-    case ClientUpdateMessage(text) =>
-      Json.parse(text).validate[UpdateMessage] match {
-        case m: JsSuccess[UpdateMessage] => dbActor ! m.value
+  override def receive: Receive = ({
+    case message: RegistrationResponseMessage => getActorOrElse(Topic() :+ Communication :+ RabbitMQ, Naming, message).foreach(actorRef => actorRef ! ChannelNamesRequestMessage(CommunicationType.UPDATES))
+    case message: ChannelNamesResponseMessage =>
+      subQueue = message.queue
+      getActorOrElse(Topic() :+ Communication :+ RabbitMQ, ChannelCreating, message).foreach(_ ! SubscribingChannelCreationMessage(message.exchange, subQueue.get, None))
+    case message: ChannelCreatedMessage => getActorOrElse(Topic() :+ Communication :+ RabbitMQ, Subscribing, message).foreach(_ ! SubscribeMessage(message.channel, subQueue.get))
+    case message: ClientUpdateMessage =>
+      Json.parse(message.text).validate[UpdateMessage] match {
+        case updateMessage: JsSuccess[UpdateMessage] => getActorOrElse(Topic() :+ Database, Master, message).foreach(_ ! updateMessage.value)
         case error: JsError => println(error)
       }
-    case _ => println("[Updates Receiver Actor] Huh?")
-  }
+  }: Receive) orElse super[BasicActor].receive
+}
+
+object Test extends App {
+  val topic = Topic() :+ Communication :+ RabbitMQ
+  val service = Naming
+
+  val system = ActorSystem("CollaboraServer")
+  val root = system.actorOf(YellowPagesActor.rootProps())
+
+  val updatesReceiver = system.actorOf(Props(
+    new UpdatesReceiverActor(root,"Update Receiver", topic, UpdatesReceiving)), "updates-receiver")
+
+  Thread.sleep(5000)
+
+  val naming = system.actorOf(Props(new RabbitMQNamingActor(root, "Naming Actor", topic, service)), "naming")
+
+  //system.terminate()
 }

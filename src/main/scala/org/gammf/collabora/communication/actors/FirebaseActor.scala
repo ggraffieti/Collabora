336b39dd1c24ce894b7dcd8750e01ca5353d9097
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-09-11 16:32:33 +0200

    FirebaseActor refactoring and integration with yellow pages

--- src/main/scala/org/gammf/collabora/communication/actors/FirebaseActor.scala
+++ src/main/scala/org/gammf/collabora/communication/actors/FirebaseActor.scala
@@ -7,53 +7,55 @@ import org.gammf.collabora.database.actors.ConnectionManagerActor
 import org.gammf.collabora.database.actors.worker.DBWorkerGetCollaborationActor
 import org.gammf.collabora.database.messages.GetCollaboration
 import org.gammf.collabora.util.{Firebase, UpdateMessage, UpdateMessageTarget, UpdateMessageType}
+import org.gammf.collabora.yellowpages.ActorService.ActorService
+import org.gammf.collabora.yellowpages.actors.BasicActor
+import org.gammf.collabora.yellowpages.util.Topic.ActorTopic
 
 /**
   * This is an actor that sends Firebase notification about Note,Module,Member operation
   * to all the client that are registered in the specific collaborationID Topic
   */
-class FirebaseActor() extends Actor{
+class FirebaseActor(override val yellowPages: ActorRef, override val name: String,
+                    override val topic: ActorTopic, override val service: ActorService) extends BasicActor{
 
-  private final val AUTHORIZATION = "AAAAJtSw2Gk:APA91bEXmB5sRFqSnuYIP3qofHQ0RfHrAzTllJ0vYWtHXKZsMdbuXmUKbr16BVZsMO0cMmm_BWE8oLzkFcyuMr_V6O6ilqvLu7TrOgirVES51Ux9PsKfJ17iOMvTF_WtwqEURqMGBbLf"
-  private[this] var info: Option[UpdateMessage] = None
+  private[this] final val AUTHORIZATION = "AAAAJtSw2Gk:APA91bEXmB5sRFqSnuYIP3qofHQ0RfHrAzTllJ0vYWtHXKZsMdbuXmUKbr16BVZsMO0cMmm_BWE8oLzkFcyuMr_V6O6ilqvLu7TrOgirVES51Ux9PsKfJ17iOMvTF_WtwqEURqMGBbLf"
   private[this] val firebase: Firebase = new Firebase
 
-  override def receive:Receive = {
-    case PublishNotificationMessage(collaborationID, message) =>
-      firebase.setKey(AUTHORIZATION)
-      info = Some(message)
-      firebase.setTtile(collaborationID)
-      firebase.setBody(setUserAndOperation()+setTextTarget())
-      firebase.to(collaborationID)
-      firebase.send()
-      //collaborationGetter ! GetCollaboration(collaborationID)
-    /*case PublishFirebaseNotification(collaborationID,collaboration)=>
-      firebase.setTtile(collaboration.name)
-      firebase.setBody(setUserAndOperation()+setTextTarget())
-      firebase.to(collaborationID)
-      firebase.send()*/
-    case _ => println("[FirebaseActor] Huh?")
+  override def receive:Receive = ({
+    case PublishNotificationMessage(collaborationID, message) => message.target match {
+            case UpdateMessageTarget.NOTE |
+                 UpdateMessageTarget.MODULE |
+                 UpdateMessageTarget.MEMBER if message.messageType.equals(UpdateMessageType.CREATION) => sendFirebaseNotification(collaborationID, message)
+            case _=>
+    }
+  }: Receive) orElse super[BasicActor].receive
+
+  private[this] def sendFirebaseNotification(collaborationID: String, message: UpdateMessage) {
+    firebase.setKey(AUTHORIZATION)
+    firebase.setTtile(collaborationID)
+    firebase.setBody(setUserAndOperation(message) + setTextTarget(message))
+    firebase.to(collaborationID)
+    firebase.send()
   }
 
   /**
     * Method used to insert in the message the User and the operation that the user requested
     * @return
     */
-  private def setUserAndOperation(): String = {
-    val notifyText: String = info.get.user
-      info.get.messageType match {
-        case UpdateMessageType.CREATION => notifyText + " added"
-        case UpdateMessageType.UPDATING => notifyText + " updated"
-        case UpdateMessageType.DELETION => notifyText + " deleted"
-      }
+  private def setUserAndOperation(message: UpdateMessage): String = {
+    message.user + (message.messageType match {
+      case UpdateMessageType.CREATION => " added"
+      case UpdateMessageType.UPDATING => " updated"
+      case UpdateMessageType.DELETION => " deleted"
+    })
   }
 
   /**
     * Method used to insert in the message the object of the requested operation
     * @return
     */
-  private def setTextTarget(): String = {
-    info.get.target match {
+  private def setTextTarget(message: UpdateMessage): String = {
+    message.target match {
       case UpdateMessageTarget.NOTE => " a note"
       case UpdateMessageTarget.MODULE => " a module"
       case UpdateMessageTarget.MEMBER=> " a member"
@@ -63,7 +65,7 @@ class FirebaseActor() extends Actor{
 }
 
 object UseFirebaseActor extends App{
-
+  //TODO refactoring
   implicit val system: ActorSystem = ActorSystem()
   val factory = new ConnectionFactory()
   var connectionManagerActor: ActorRef = system.actorOf(Props[ConnectionManagerActor])
@@ -71,10 +73,10 @@ object UseFirebaseActor extends App{
   val naming: ActorRef = system.actorOf(Props[RabbitMQNamingActor], "naming")
   val channelCreator: ActorRef = system.actorOf(Props[ChannelCreatorActor], "channelCreator")
   val publisher: ActorRef = system.actorOf(Props[PublisherActor], "publisher")
-  val collaborationMember: ActorRef = system.actorOf(Props(
-    new CollaborationMembersActor(connection, naming, channelCreator, publisher)), "collaboration-members")
-  var getCollaborarionsActor:ActorRef = system.actorOf(Props.create(classOf[DBWorkerGetCollaborationActor], connectionManagerActor, collaborationMember))
-  var firebaseActor: ActorRef = system.actorOf(Props.create(classOf[FirebaseActor], getCollaborarionsActor))
+  //val collaborationMember: ActorRef = system.actorOf(Props(
+  //  new CollaborationMembersActor(connection, naming, channelCreator, publisher)), "collaboration-members")
+  //var getCollaborarionsActor:ActorRef = system.actorOf(Props.create(classOf[DBWorkerGetCollaborationActor], connectionManagerActor, collaborationMember))
+  //var firebaseActor: ActorRef = system.actorOf(Props.create(classOf[FirebaseActor], getCollaborarionsActor))
 
-  firebaseActor ! PublishNotificationMessage("123456788698540008900400",UpdateMessage(UpdateMessageTarget.NOTE,UpdateMessageType.UPDATING,"maffone"))
+  //firebaseActor ! PublishNotificationMessage("123456788698540008900400",UpdateMessage(UpdateMessageTarget.NOTE,UpdateMessageType.UPDATING,"maffone"))
 }

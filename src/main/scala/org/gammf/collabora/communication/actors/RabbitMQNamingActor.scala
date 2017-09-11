31a10b2973c4d9cd6a9b214234fa7313045212a3
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-09-11 16:31:03 +0200

    Simple integration with yellow pages for those actors that didn't have actor references among their fields

--- src/main/scala/org/gammf/collabora/communication/actors/RabbitMQNamingActor.scala
+++ src/main/scala/org/gammf/collabora/communication/actors/RabbitMQNamingActor.scala
@@ -1,8 +1,11 @@
 package org.gammf.collabora.communication.actors
 
-import akka.actor.Actor
+import akka.actor.ActorRef
 import org.gammf.collabora.communication.Utils.CommunicationType
 import org.gammf.collabora.communication.messages.{ChannelNamesRequestMessage, ChannelNamesResponseMessage}
+import org.gammf.collabora.yellowpages.ActorService.ActorService
+import org.gammf.collabora.yellowpages.actors.BasicActor
+import org.gammf.collabora.yellowpages.util.Topic.ActorTopic
 
 /**
   * @author Manuel Peruzzi
@@ -11,12 +14,12 @@ import org.gammf.collabora.communication.messages.{ChannelNamesRequestMessage, C
 /**
   * This is an actor that handles the rabbitMQ naming issues.
   */
-class RabbitMQNamingActor extends Actor {
+class RabbitMQNamingActor(override val yellowPages: ActorRef, override val name: String,
+                          override val topic: ActorTopic, override val service: ActorService) extends BasicActor {
 
-  override def receive: Receive = {
+  override def receive: Receive = ({
     case ChannelNamesRequestMessage(commType: CommunicationType.Value) => sender ! names2Message(commType)
-    case _ => println("Huh?")
-  }
+  }:Receive) orElse super[BasicActor].receive
 
   private[this] implicit def type2Names(commType: CommunicationType.Value): CommunicationNames =
     commType match {

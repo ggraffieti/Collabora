289e7b9b3a161d4baaed94064c7fb401821cd33b
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-09-12 21:33:19 +0200

    DB(sub)Masters integration with yellow pages

--- src/main/scala/org/gammf/collabora/database/actors/master/DBMasterCollaboration.scala
+++ src/main/scala/org/gammf/collabora/database/actors/master/DBMasterCollaboration.scala
@@ -5,7 +5,12 @@ import org.gammf.collabora.communication.messages.{PublishCollaborationInCollabo
 import org.gammf.collabora.database.actors.worker.DBWorkerCollaborationsActor
 import org.gammf.collabora.database.messages._
 import org.gammf.collabora.util.{CollaborationMessage, CollaborationType, ServerErrorCode, ServerErrorMessage, UpdateMessage, UpdateMessageTarget, UpdateMessageType}
+import org.gammf.collabora.yellowpages.ActorService.ActorService
+import org.gammf.collabora.yellowpages.util.Topic.ActorTopic
 
+import org.gammf.collabora.yellowpages.util.Topic
+import org.gammf.collabora.yellowpages.TopicElement._
+import org.gammf.collabora.yellowpages.ActorService._
 /**
   * The master actor that manages all the query about collaborations.
   * @param system the actor system, used for create the needed workers.
@@ -14,47 +19,48 @@ import org.gammf.collabora.util.{CollaborationMessage, CollaborationType, Server
   * @param notificationActor The actor used for notify the client that a query is went good.
   * @param publishCollaborationExchangeActor the actor used for send notifications in the collaboration exchange.
   */
-class DBMasterCollaboration(system: ActorSystem, connectionManagerActor: ActorRef, notificationActor: ActorRef, publishCollaborationExchangeActor: ActorRef) extends AbstractDBMaster {
+class DBMasterCollaboration(override val yellowPages: ActorRef, override val name: String,
+                            override val topic: ActorTopic, override val service: ActorService) extends AbstractDBMaster {
 
-  private[this] var collaborationWorker: ActorRef = _
-
-  override def preStart(): Unit = {
-    collaborationWorker = system.actorOf(Props.create(classOf[DBWorkerCollaborationsActor], connectionManagerActor))
-  }
-
-  override def receive: Receive = {
+  override def receive: Receive = ({
 
     case message: UpdateMessage => message.target match {
       case UpdateMessageTarget.COLLABORATION => message.messageType match {
         case UpdateMessageType.CREATION =>
           if (message.collaboration.get.collaborationType == CollaborationType.PRIVATE)
-            collaborationWorker forward InsertCollaborationMessage(message.collaboration.get, message.user)
+            getActorOrElse(Topic() :+ Database :+ Collaboration, DefaultWorker, message).foreach(_ forward InsertCollaborationMessage(message.collaboration.get, message.user))
           else
-            collaborationWorker ! InsertCollaborationMessage(message.collaboration.get, message.user)
-        case UpdateMessageType.UPDATING => collaborationWorker ! UpdateCollaborationMessage(message.collaboration.get, message.user)
-        case UpdateMessageType.DELETION => collaborationWorker ! DeleteCollaborationMessage(message.collaboration.get, message.user)
+            getActorOrElse(Topic() :+ Database :+ Collaboration, DefaultWorker, message).foreach(_ ! InsertCollaborationMessage(message.collaboration.get, message.user))
+        case UpdateMessageType.UPDATING => getActorOrElse(Topic() :+ Database :+ Collaboration, DefaultWorker, message).foreach(_ ! UpdateCollaborationMessage(message.collaboration.get, message.user))
+        case UpdateMessageType.DELETION => getActorOrElse(Topic() :+ Database :+ Collaboration, DefaultWorker, message).foreach(_ ! DeleteCollaborationMessage(message.collaboration.get, message.user))
       }
     }
 
-    case QueryOkMessage(queryGoneWell) => queryGoneWell match {
+    case message @ (_:GetAllCollaborationsMessage | _:GetCollaborationMessage) =>
+      getActorOrElse(Topic() :+ Database :+ Collaboration, Getter, message).foreach(_ forward message)
+
+    case message: QueryOkMessage => message.queryGoneWell match {
       case query: QueryCollaborationMessage => query match {
-        case _: InsertCollaborationMessage => publishCollaborationExchangeActor ! PublishCollaborationInCollaborationExchange(query.userID, CollaborationMessage(user=query.userID,collaboration = query.collaboration))
-        case _ => notificationActor ! PublishNotificationMessage(query.collaboration.id.get, UpdateMessage(
-          target = UpdateMessageTarget.COLLABORATION,
-          messageType = getUpdateTypeFromQueryMessage(query),
-          user = query.userID,
-          collaboration = Some(query.collaboration),
-          collaborationId = Some(query.collaboration.id.get)))
+        case _: InsertCollaborationMessage =>
+          getActorOrElse(Topic() :+ Communication :+ Collaborations :+ RabbitMQ , Master, message)
+            .foreach(_ ! PublishCollaborationInCollaborationExchange(query.userID, CollaborationMessage(user=query.userID,collaboration = query.collaboration)))
+        case _ =>
+          getActorOrElse(Topic() :+ Communication :+ Notifications, Bridging, message)
+            .foreach(_ ! PublishNotificationMessage(query.collaboration.id.get, UpdateMessage(
+              target = UpdateMessageTarget.COLLABORATION,
+              messageType = getUpdateTypeFromQueryMessage(query),
+              user = query.userID,
+              collaboration = Some(query.collaboration),
+              collaborationId = Some(query.collaboration.id.get))))
       }
       case _ => unhandled(_)
     }
 
-    case fail: QueryFailMessage => publishCollaborationExchangeActor ! PublishErrorMessageInCollaborationExchange(
-      username = fail.username,
-      message = ServerErrorMessage(user = fail.username, errorCode = ServerErrorCode.SERVER_ERROR)
-    )
+    case fail: QueryFailMessage => getActorOrElse(Topic() :+ Communication :+ Collaborations :+ RabbitMQ, Master, fail)
+      .foreach(_ ! PublishErrorMessageInCollaborationExchange(
+        username = fail.username,
+        message = ServerErrorMessage(user = fail.username, errorCode = ServerErrorCode.SERVER_ERROR)
+      ))
 
-    case _ => unhandled(_)
-
-  }
+  }: Receive) orElse super[AbstractDBMaster].receive
 }

93c950ca75bbf5be40efe65ee034d47acc614a56
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-09-12 21:34:04 +0200

    ConnectionManagerActor integration with yellow pages

--- src/main/scala/org/gammf/collabora/database/actors/ConnectionManagerActor.scala
+++ src/main/scala/org/gammf/collabora/database/actors/ConnectionManagerActor.scala
@@ -1,16 +1,20 @@
 package org.gammf.collabora.database.actors
 
-import akka.actor.{Actor, Stash}
+import akka.actor.{Actor, ActorRef, Stash}
 import org.gammf.collabora.database.messages.{AskConnectionMessage, GetConnectionMessage}
 import org.gammf.collabora.database._
+import org.gammf.collabora.yellowpages.actors.BasicActor
 import reactivemongo.api.{MongoConnection, MongoDriver}
 
 import scala.util.{Failure, Success}
+import org.gammf.collabora.yellowpages.util.Topic._
+import org.gammf.collabora.yellowpages.ActorService._
 
 /**
   * An actor that have the purpose of establishing and mantaining the connection with the Database
   */
-class ConnectionManagerActor extends Actor with Stash {
+class ConnectionManagerActor(override val yellowPages: ActorRef, override val name: String,
+                             override val topic: ActorTopic, override val service: ActorService) extends BasicActor with Stash {
 
   private[this] var connection: Option[MongoConnection] = None
   private[this] val mongoUri = CONNECTION_STRING
@@ -23,10 +27,10 @@ class ConnectionManagerActor extends Actor with Stash {
       case Success(c) => connection = Some(c)
       case Failure(e) => e.printStackTrace() // TODO now logging stack
     }
+    super[BasicActor].preStart()
   }
 
-  override def receive: Receive = {
+  override def receive: Receive = ({
     case _ : AskConnectionMessage => sender ! GetConnectionMessage(connection.get)
-    case _ => unhandled(_)
-  }
+  }: Receive) orElse super[BasicActor].receive
 }

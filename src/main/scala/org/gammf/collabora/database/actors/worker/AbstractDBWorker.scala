2d5d654d5735eb40b381d20c1a7a7f98c36a56c4
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-09-12 21:42:52 +0200

    DBworker traits and abstract classes integration with yellow pages

--- src/main/scala/org/gammf/collabora/database/actors/worker/AbstractDBWorker.scala
+++ src/main/scala/org/gammf/collabora/database/actors/worker/AbstractDBWorker.scala
@@ -1,8 +1,6 @@
 package org.gammf.collabora.database.actors.worker
 
-import akka.actor.ActorRef
 import org.gammf.collabora.database._
-import org.gammf.collabora.database.messages.AskConnectionMessage
 import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.api.{FailoverStrategy, MongoConnection}
 
@@ -14,11 +12,11 @@ import scala.concurrent.Future
   * @param connectionActor the actor that mantains the connection with the DB.
   * @tparam T the type returned by query methods, in case of query gone good or bad.
   */
-abstract class AbstractDBWorker[T](val connectionActor: ActorRef) extends DBWorker[T] {
+abstract class AbstractDBWorker[T] extends DBWorker[T] {
 
   protected var connection: Option[MongoConnection] = None
 
-  override def preStart(): Unit = connectionActor ! AskConnectionMessage()
+  override def receive: Receive = super[DBWorker].receive
 
   protected def getCollaborationsCollection: Future[BSONCollection] =
     connection.get.database(DB_NAME, FailoverStrategy())

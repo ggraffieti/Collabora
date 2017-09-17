344e7f2ce80a0253c4a3541406ecffca25ddf682
Alfredo Maffi <alfredo.maffi@studio.unibo.it>
2017-09-17 17:07:55 +0200

    Test fix for dbMasterActor

--- src/test/scala/org/gammf/collabora/database/actors/DBMasterActorTest.scala
+++ src/test/scala/org/gammf/collabora/database/actors/DBMasterActorTest.scala
@@ -1,18 +1,16 @@
 package org.gammf.collabora.database.actors
 
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.pattern.ask
 import akka.testkit.{ImplicitSender, TestKit}
 import akka.util.Timeout
-import com.newmotion.akka.rabbitmq.{ConnectionActor, ConnectionFactory}
-import com.rabbitmq.client.{AMQP, BuiltinExchangeType, DefaultConsumer, Envelope}
+import org.gammf.collabora.communication.messages.PublishErrorMessageInCollaborationExchange
 import org.gammf.collabora.{TestMessageUtil, TestUtil}
-import org.gammf.collabora.communication.actors._
-import org.gammf.collabora.communication.messages.{ClientUpdateMessage, StartMessage}
-import org.gammf.collabora.database.actors.master.DBMasterActor
-import org.gammf.collabora.yellowpages.ActorCreator
-import org.gammf.collabora.yellowpages.ActorService.Master
-import org.gammf.collabora.yellowpages.messages.{ActorRequestMessage, ActorResponseMessage, ActorResponseOKMessage}
+import org.gammf.collabora.database.messages.{GetAllCollaborationsMessage, GetCollaborationMessage, QueryFailMessage}
+import org.gammf.collabora.util.{Collaboration, CollaborationUser, Module, Note, ServerErrorCode, ServerErrorMessage, UpdateMessage}
+import org.gammf.collabora.yellowpages.{ActorContainer, TopicElement}
+import org.gammf.collabora.yellowpages.ActorService._
+import org.gammf.collabora.yellowpages.messages._
 import org.gammf.collabora.yellowpages.util.Topic
 import org.gammf.collabora.yellowpages.TopicElement._
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
@@ -20,66 +18,38 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import scala.concurrent.duration._
 import org.scalatest.concurrent.Eventually
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.language.postfixOps
 
 
-class DBMasterActorTest extends TestKit (ActorSystem("CollaboraServer")) with WordSpecLike with Eventually with Matchers with BeforeAndAfterAll with ImplicitSender {
+class DBMasterActorTest extends TestKit (ActorSystem("CollaboraTest")) with WordSpecLike with Eventually with Matchers with BeforeAndAfterAll with ImplicitSender {
 
-  private val EXCHANGE_NAME = "notifications"
-  private val ROUTING_KEY = "59806a4af27da3fcfe0ac0ca"
-  private val BROKER_HOST = "localhost"
-
-  var msg: String = ""
+  var expectedNote: Option[Note] = None
+  var expectedModule: Option[Module] = None
+  var expectedCollaboration: Option[Collaboration] = None
+  var expectedCollaborationString: String = ""
+  var expectedCollaborationsString: String = ""
+  var expectedMember: Option[CollaborationUser] = None
+  var expectedDBError: Option[ServerErrorMessage] = None
 
   implicit protected[this] val askTimeout: Timeout = Timeout(5 second)
-  val actorCreator = new ActorCreator(system)
-  actorCreator.startCreation
-  val rootYellowPages = actorCreator.getYellowPagesRoot
-
-  /*
-   val CONNECTION_ACTOR_NAME = "rabbitmq"
-  val NAMING_ACTOR_NAME = "naming"
-  val CHANNEL_CREATOR_NAME = "channelCreator"
-  val PUBLISHER_ACTOR_NAME = "publisher"
-  val UPDATES_RECEIVER_ACTOR_NAME = "updates-receiver"
-  val SUBSCRIBER_ACTOR_NAME = "subscriber"
-
-  val dbConnectionActor: ActorRef = system.actorOf(Props[ConnectionManagerActor])
-  val factory = new ConnectionFactory()
-  val connection:ActorRef = system.actorOf(ConnectionActor.props(factory), CONNECTION_ACTOR_NAME)
-  val naming: ActorRef = system.actorOf(Props[RabbitMQNamingActor], NAMING_ACTOR_NAME)
-  val channelCreator: ActorRef = system.actorOf(Props[ChannelCreatorActor], CHANNEL_CREATOR_NAME)
-  val publisherActor: ActorRef = system.actorOf(Props[PublisherActor], PUBLISHER_ACTOR_NAME)
-  val collaborationMemberActor:ActorRef = system.actorOf(Props(
-    new CollaborationMembersActor(connection, naming, channelCreator, publisherActor)))
-  val notificationActor: ActorRef = system.actorOf(Props(new NotificationsSenderActor(connection, naming, channelCreator, publisherActor,system)))
-  val dbMasterActor:ActorRef = system.actorOf(Props.create(classOf[DBMasterActor], system, notificationActor,collaborationMemberActor))
-  val subscriber:ActorRef = system.actorOf(Props[SubscriberActor], SUBSCRIBER_ACTOR_NAME)
-  val updatesReceiver:ActorRef = system.actorOf(Props(
-    new UpdatesReceiverActor(connection, naming, channelCreator, subscriber, dbMasterActor)), UPDATES_RECEIVER_ACTOR_NAME)
-*/
-
+  var rootYellowPages: ActorRef = _
 
   override def beforeAll(): Unit = {
-    val factory = new ConnectionFactory
-    factory.setHost(TestUtil.BROKER_HOST)
-    val connection = factory.newConnection
-    val channel = connection.createChannel
-    channel.exchangeDeclare(TestUtil.TYPE_NOTIFICATIONS, BuiltinExchangeType.DIRECT, true)
-    val queueName = channel.queueDeclare.getQueue
-    channel.queueBind(queueName, TestUtil.TYPE_NOTIFICATIONS, TestUtil.ROUTING_KEY)
-    val consumer = new DefaultConsumer(channel) {
-      override def handleDelivery(consumerTag: String, envelope: Envelope, properties: AMQP.BasicProperties, body: Array[Byte]): Unit = {
-        msg = new String(body, TestUtil.STRING_ENCODING)
-      }
-    }
-    channel.basicConsume(queueName, true, consumer)
+    ActorContainer.init()
+    ActorContainer.createAll()
+    rootYellowPages = ActorContainer.rootYellowPages
 
-    //updatesReceiver ! StartMessage
-    //notificationActor ! StartMessage
+    deregisterDBMasters()
+    registerFakeDBMasters()
 
+    deregisterCollaborationMemberActor()
+    registerFakeCollaborationMemberActor()
+
+    Thread.sleep(200)
   }
   override def afterAll(): Unit = {
+    ActorContainer.shutdown()
     TestKit.shutdownActorSystem(system)
   }
 
@@ -90,22 +60,169 @@ class DBMasterActorTest extends TestKit (ActorSystem("CollaboraServer")) with Wo
 
   "A DBMaster actor" should {
 
- /*   "act like a gateway for every request from and to the DB" in {
-      val message = TestMessageUtil.messageDBMasterActorTest
-      //updatesReceiver ! ClientUpdateMessage(message)
-      (rootYellowPages ? ActorRequestMessage(Topic() :+ Communication :+ Updates :+ RabbitMQ, Master))
-        .mapTo[ActorResponseMessage].map {
-        case response: ActorResponseOKMessage => response.actor ! ClientUpdateMessage(message)
-        case _ =>
+    "act like a gateway for every request to the DB" in {
+     Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database, Master), askTimeout.duration)
+       .asInstanceOf[ActorResponseMessage] match {
+       case response: ActorResponseOKMessage =>
+         response.actor ! TestMessageUtil.noteUpdateMessage
+       case _ => fail
+
+     }
+     eventually {
+       expectedNote should not be None
+     }
+     assert(expectedNote.get == TestMessageUtil.notificationNote)
+
+     Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database, Master), askTimeout.duration)
+       .asInstanceOf[ActorResponseMessage] match {
+       case response: ActorResponseOKMessage =>
+         response.actor ! TestMessageUtil.moduleUpdateMessage
+       case _ => fail
+
+     }
+     eventually {
+       expectedModule should not be None
+     }
+     assert(expectedModule.get == TestMessageUtil.notificationModule)
+
+     Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database, Master), askTimeout.duration)
+       .asInstanceOf[ActorResponseMessage] match {
+       case response: ActorResponseOKMessage =>
+         response.actor ! TestMessageUtil.collaborationUpdateMessage
+       case _ => fail
+
+     }
+     eventually {
+       expectedCollaboration should not be None
+     }
+     assert(expectedCollaboration.get == TestMessageUtil.notificationCollaboration)
+
+     Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database, Master), askTimeout.duration)
+       .asInstanceOf[ActorResponseMessage] match {
+       case response: ActorResponseOKMessage =>
+         response.actor ! TestMessageUtil.memberUpdateMessage
+       case _ => fail
+
+     }
+     eventually {
+       expectedMember should not be None
+     }
+     assert(expectedMember.get == TestMessageUtil.notificationMember)
+
+     Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database, Master), askTimeout.duration)
+       .asInstanceOf[ActorResponseMessage] match {
+       case response: ActorResponseOKMessage =>
+         response.actor ! GetCollaborationMessage("1234567890")
+       case _ => fail
+
+     }
+     eventually {
+       expectedCollaborationString should not be ""
+     }
+     assert(expectedCollaborationString == "1234567890")
+
+     Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database, Master), askTimeout.duration)
+       .asInstanceOf[ActorResponseMessage] match {
+       case response: ActorResponseOKMessage =>
+         response.actor ! GetAllCollaborationsMessage("fone")
+       case _ => fail
+
+     }
+     eventually {
+       expectedCollaborationsString should not be ""
+     }
+     assert(expectedCollaborationsString == "fone")
+   }
+
+    "contact the CollaborationMember actor whenever a QueryFail message is received" in {
+      Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database, Master), askTimeout.duration)
+        .asInstanceOf[ActorResponseMessage] match {
+        case response: ActorResponseOKMessage =>
+          response.actor ! QueryFailMessage(new Exception, "fone")
+        case _ => fail
 
       }
-      eventually{
-        msg should not be ""
+      eventually {
+        expectedDBError should not be None
       }
-      val contain = TestMessageUtil.messageToBeContainedDBMasterActorTest
-      assert(msg.contains(contain))
+      assert(expectedDBError.get == ServerErrorMessage("fone", ServerErrorCode.SERVER_ERROR))
     }
-    */
   }
 
+  private def deregisterDBMasters():Unit = {
+    Thread.sleep(200)
+    Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database :+ TopicElement.Note, Master), askTimeout.duration)
+      .asInstanceOf[ActorResponseMessage] match {
+      case response: ActorResponseOKMessage => rootYellowPages ! DeletionRequestMessage(response.actor, "DBMasterNotes", Topic() :+ Database :+ TopicElement.Note, Master)
+      case _ => fail
+    }
+
+    Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database :+ TopicElement.Module, Master), askTimeout.duration)
+      .asInstanceOf[ActorResponseMessage] match {
+      case response: ActorResponseOKMessage => rootYellowPages ! DeletionRequestMessage(response.actor, "DBMasterModules", Topic() :+ Database :+ TopicElement.Module, Master)
+      case _ => fail
+    }
+
+    Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database :+ TopicElement.Collaboration, Master), askTimeout.duration)
+      .asInstanceOf[ActorResponseMessage] match {
+      case response: ActorResponseOKMessage => rootYellowPages ! DeletionRequestMessage(response.actor, "DBMasterCollaborations", Topic() :+ Database :+ TopicElement.Collaboration, Master)
+      case _ => fail
+    }
+
+    Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Database :+ Member, Master), askTimeout.duration)
+      .asInstanceOf[ActorResponseMessage] match {
+      case response: ActorResponseOKMessage => rootYellowPages ! DeletionRequestMessage(response.actor, "DBMasterMembers", Topic() :+ Database :+ Member, Master)
+      case _ => fail
+    }
+  }
+
+  private def registerFakeDBMasters():Unit = {
+    val fakeDBNoteMaster = ActorContainer.actorSystem.actorOf(Props(new Actor {
+      override def receive: Receive = {
+        case message: UpdateMessage => expectedNote = message.note
+      }
+    }))
+    rootYellowPages ! RegistrationRequestMessage(fakeDBNoteMaster, "fakeDBNoteMaster", Topic() :+ Database :+ TopicElement.Note, Master)
+
+    val fakeDBModuleMaster = ActorContainer.actorSystem.actorOf(Props(new Actor {
+      override def receive: Receive = {
+        case message: UpdateMessage => expectedModule = message.module
+      }
+    }))
+    rootYellowPages ! RegistrationRequestMessage(fakeDBModuleMaster, "fakeDBModuleMaster", Topic() :+ Database :+ TopicElement.Module, Master)
+
+    val fakeDBCollaborationMaster = ActorContainer.actorSystem.actorOf(Props(new Actor {
+      override def receive: Receive = {
+        case message: UpdateMessage => expectedCollaboration = message.collaboration
+        case message: GetAllCollaborationsMessage =>expectedCollaborationsString = message.username
+        case message: GetCollaborationMessage => expectedCollaborationString = message.collaborationID
+      }
+    }))
+    rootYellowPages ! RegistrationRequestMessage(fakeDBCollaborationMaster, "fakeDBCollaborationMaster", Topic() :+ Database :+ TopicElement.Collaboration, Master)
+
+    val fakeDBMemberMaster = ActorContainer.actorSystem.actorOf(Props(new Actor {
+      override def receive: Receive = {
+        case message: UpdateMessage => expectedMember = message.member
+      }
+    }))
+    rootYellowPages ! RegistrationRequestMessage(fakeDBMemberMaster, "fakeDBMemberMaster", Topic() :+ Database :+ TopicElement.Member, Master)
+
+  }
+
+  private def deregisterCollaborationMemberActor(): Unit = {
+    Await.result(rootYellowPages ? ActorRequestMessage(Topic() :+ Communication :+ Collaborations :+ RabbitMQ, Master), askTimeout.duration)
+      .asInstanceOf[ActorResponseMessage] match {
+      case response: ActorResponseOKMessage => rootYellowPages ! DeletionRequestMessage(response.actor, "CollaborationActor", Topic() :+ Communication :+ Collaborations :+ RabbitMQ, Master)
+      case _ => fail
+    }
+  }
+
+  private def registerFakeCollaborationMemberActor(): Unit = {
+    val fakeCollaborationMemberActor = ActorContainer.actorSystem.actorOf(Props(new Actor {
+      override def receive: Receive = {
+        case message: PublishErrorMessageInCollaborationExchange => expectedDBError = Some(message.message)
+      }
+    }))
+    rootYellowPages ! RegistrationRequestMessage(fakeCollaborationMemberActor, "fakeCollaborationMemberActor", Topic() :+ Communication :+ Collaborations :+ RabbitMQ, Master)
+  }
 }
